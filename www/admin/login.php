3700aa5017adb550b7bce25695a282920d99138d
bohwaz <bohwaz@kd2.org>
2012-04-02 23:18:43 +0000

    Fix [fcfc2b9395] : garder la session en vie tant que la page est dans un onglet
    FossilOrigin-Name: 71b6152ae3e3d58459ef21f988db03ff234bda9d

--- www/admin/login.php
+++ www/admin/login.php
@@ -3,6 +3,20 @@
 define('GARRADIN_LOGIN_PROCESS', true);
 require_once __DIR__ . '/_inc.php';
 
+// Relance session_start et renvoie une image de 1px transparente
+if (isset($_GET['keepSessionAlive']))
+{
+    $membres->keepSessionAlive();
+
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+
+    header('Content-Type: image/gif');
+    echo base64_decode("R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==");
+
+    exit;
+}
+
 $error = false;
 
 if (!empty($_POST['login']))
