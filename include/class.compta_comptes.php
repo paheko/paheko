2eaced6105d68e0879e52aace3ad565c8ac771f7
bohwaz <bohwaz@noemail.net>
2012-07-05 15:24:45 +0000

    Débuts compta : objet pour les comptes + schéma DB de départ
    FossilOrigin-Name: 42591ee84f61ea4ded00c7d95590b8c0b220b82f

---
+++ include/class.compta_comptes.php
@@ -0,0 +1,123 @@
+<?php
+
+class Garradin_Compta_Comptes
+{
+    public function importPlan()
+    {
+        $plan = json_decode(file_get_contents(GARRADIN_ROOT . '/include/plan_comptable.json'));
+
+        $db = Garradin_DB::getInstance();
+        $db->exec('BEGIN;');
+        $db->exec('DELETE FROM compta_comptes WHERE plan_comptable = 1;'); // Nettoyage avant insertion
+
+        foreach ($plan as $id=>$compte)
+        {
+            $db->simpleInsert('compta_comptes', array(
+                'id'        =>  $id,
+                'parent'    =>  $compte['parent'],
+                'libelle'   =>  $compte['nom'],
+                'plan_comptable' => 1,
+            ));
+        }
+
+        $db->exec('END;');
+
+        return true;
+    }
+
+    public function add($data)
+    {
+        $this->_checkFields($data);
+
+        $db = Garradin_DB::getInstance();
+
+        $new_id = $data['parent'];
+        $nb_sous_comptes = $db->simpleQuerySingle('SELECT COUNT(*) FROM compta_comptes WHERE parent = ?;');
+
+        // Pas plus de 26 sous-comptes par compte, parce que l'alphabet s'arrête à 26 lettres
+        if ($nb_sous_comptes >= 26)
+        {
+            throw new UserException('Nombre de sous-comptes maximal atteint pour ce compte parent-ci.');
+        }
+
+        $new_id .= chr(65+(int)$nb_sous_comptes);
+
+        $db->simpleInsert('compta_comptes', array(
+            'id'        =>  $new_id,
+            'libelle'   =>  trim($data['libelle']),
+            'parent'    =>  $data['parent'],
+            'plan_comptable' => 0,
+        ));
+
+        return $new_id;
+    }
+
+    public function edit($id, $data)
+    {
+        $db = Garradin_DB::getInstance();
+
+        // Vérification que l'on peut éditer ce compte
+        if ($db->simpleQuerySingle('SELECT plan_comptable FROM compta_comptes WHERE id = ?;', false, $id))
+        {
+            throw new UserException('Ce compte fait partie du plan comptable et n\'est pas modifiable.');
+        }
+
+        $this->_checkFields($data);
+
+        $db->simpleUpdate('compta_comptes', array('libelle' => trim($data['libelle'])),
+            'id = \''.trim($id).'\'');
+
+        return true;
+    }
+
+    public function delete($id)
+    {
+        $db = Garradin_DB::getInstance();
+
+        // Ne pas supprimer un compte qui est utilisé !
+        if ($db->simpleQuerySingle('SELECT 1 FROM compta_journal WHERE compte_debit = ? OR compte_debit = ? LIMIT 1;', false, $id, $id))
+        {
+            throw new UserException('Ce compte ne peut être supprimé car des opérations comptables y sont liées.');
+        }
+
+        $db->simpleExec('DELETE FROM compta_comptes WHERE id = ?;', $id);
+        $db->simpleExec('DELETE FROM compta_comptes_bancaires WHERE id = ?;', $id);
+
+        return true;
+    }
+
+    public function get($id)
+    {
+        $db = Garradin_DB::getInstance();
+        return $db->simpleQuerySingle('SELECT * FROM compta_comptes WHERE id = ?;', true, $id);
+    }
+
+    protected function _checkFields(&$data)
+    {
+        $db = Garradin_DB::getInstance();
+
+        if (empty($data['libelle']) || !trim($data['libelle']))
+        {
+            throw new UserException('Le libellé ne peut rester vide.');
+        }
+
+        if (isset($data['parent']))
+        {
+            if (!trim($data['parent']))
+            {
+                throw new UserException('Le compte ne peut pas ne pas avoir de compte parent.');
+            }
+
+            if (!($id = $db->simpleQuerySingle('SELECT id FROM compta_comptes WHERE id = ? AND plan_comptable = 1;', false, $data['parent'])))
+            {
+                throw new UserException('Le compte parent indiqué n\'existe pas.');
+            }
+
+            $data['parent'] = $id;
+        }
+
+        return true;
+    }
+}
+
+?>
