6a40a920bc4809b3e847a4f2f06a79e1a3c0f604
bohwaz <bohwaz@kd2.org>
2012-08-13 00:12:43 +0000

    Un peu mieux comme ça mais ça manque toujours de factorisation/simplification
    FossilOrigin-Name: 5df677653797b099684ebdb1b8776e833cee9a8a

--- www/admin/compta/graph.php
+++ www/admin/compta/graph.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__ . '/_inc.php';
 
-require_once GARRADIN_ROOT . '/include/class.compta_journal.php';
-$journal = new Garradin_Compta_Journal;
+require_once GARRADIN_ROOT . '/include/class.compta_stats.php';
+$stats = new Garradin_Compta_Stats;
 
 require_once GARRADIN_ROOT . '/include/libs/svgplot/lib.svgplot.php';
 
@@ -12,15 +12,24 @@ $plot = new SVGPlot(400, 300);
 if (utils::get('g') == 'recettes_depenses')
 {
 	$data = array(
-		$journal->getStatsRecettes(),
-		//$journal->getStatsDepenses(),
+		$stats->recettes(),
+		$stats->depenses(),
 	);
 }
+elseif (utils::get('g') == 'actif_passif')
+{
+	$data = array(
+		$stats->actif(),
+		$stats->passif()
+	);
+}
+
+$i = 0;
 
 foreach ($data as $line)
 {
 	$line = new SVGPlot_Data($line);
-	$line->color = '#d98628';
+	$line->color = ($i++ % 2) ? '#d98628' : '#9c4f15';
 	$line->width = 5;
 	$plot->add($line);
 }
