ad899b698b284424ea8de6f7d4327b0ff57c6860
bohwaz <bohwaz@kd2.org>
2012-09-25 02:51:59 +0000

    Grand livre
    FossilOrigin-Name: 5944491c67bcc61c90094746ead4c7bf04dcfc14

---
+++ www/admin/compta/grand_livre.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/_inc.php';
+
+require_once GARRADIN_ROOT . '/include/class.compta_journal.php';
+$journal = new Garradin_Compta_Journal;
+
+$liste_comptes = $comptes->getListAll();
+
+function get_nom_compte($compte)
+{
+	global $liste_comptes;
+	return $liste_comptes[$compte];
+}
+
+$tpl->register_modifier('get_nom_compte', 'get_nom_compte');
+$tpl->assign('livre', $journal->getGrandLivre());
+
+$tpl->display('admin/compta/grand_livre.tpl');
+
+?>
