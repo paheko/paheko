a59a394ad00cfd72e46be6bb9b168d74bd09a066
bohwaz <bohwaz@noemail.net>
2012-10-05 00:10:35 +0000

    Appel correct de _inc.php
    FossilOrigin-Name: f14c9b93ec557477baac9776f96ef6f33e67adf3

--- www/admin/compta/rapport/compte_resultat.php
+++ www/admin/compta/rapport/compte_resultat.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/_inc.php';
+require_once __DIR__ . '/../_inc.php';
 
 require_once GARRADIN_ROOT . '/include/class.compta_journal.php';
 $journal = new Garradin_Compta_Journal;
