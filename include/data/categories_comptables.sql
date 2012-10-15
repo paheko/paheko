f65d38664600be3ebe0ad697eb1b0b7b5a7d8567
bohwaz <bohwaz@kd2.org>
2012-10-15 05:26:49 +0000

    Solde cumulé
    FossilOrigin-Name: 9ffecd790a22b8b37593d4081d5e9eb7ad0f7185

--- include/data/categories_comptables.sql
+++ include/data/categories_comptables.sql
@@ -9,9 +9,13 @@ INSERT INTO "compta_categories" VALUES(NULL,-1,'Fournitures administratives','Ca
 INSERT INTO "compta_categories" VALUES(NULL,-1,'Frais d''actes et de contentieux','Insertion au Journal Officiel, frais de justice, etc.','6227');
 INSERT INTO "compta_categories" VALUES(NULL,-1,'Frais postaux et télécommunications','Facture d''accès à Internet, timbres, etc.','626');
 INSERT INTO "compta_categories" VALUES(NULL,-1,'Prime d''assurance','','616');
+INSERT INTO "compta_categories" VALUES(NULL,-1,'Services bancaires','','627');
+INSERT INTO "compta_categories" VALUES(NULL,-1,'Divers','','658');
 
 INSERT INTO "compta_categories" VALUES(NULL,1,'Vente de produits finis','Vente de produits fabriqués par l''association.','701');
 INSERT INTO "compta_categories" VALUES(NULL,1,'Prestation de service','','706');
 INSERT INTO "compta_categories" VALUES(NULL,1,'Revente de marchandises','','707');
 INSERT INTO "compta_categories" VALUES(NULL,1,'Manifestations diverses','Revenus provenant de manifestations au profit de l''association : droit d''entrée, location d''emplacement en vide grenier, ventes, etc.','7780');
 INSERT INTO "compta_categories" VALUES(NULL,1,'Cotisations','','756');
+INSERT INTO "compta_categories" VALUES(NULL,1,'Dons et collectes','','754');
+INSERT INTO "compta_categories" VALUES(NULL,1,'Divers','','758');
