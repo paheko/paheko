287eeee645e4753e2ba6e5f296a8b74a4da86b56
bohwaz <bohwaz@noemail.net>
2012-10-16 03:57:48 +0000

    preg_quote est trop généreux pour ça
    FossilOrigin-Name: 463583f6e9e576f59fe99171120aeb1d601b2cd7

--- include/class.db.php
+++ include/class.db.php
@@ -275,7 +275,8 @@ class Garradin_DB extends SQLite3
                     throw new InvalidArgumentException(__FUNCTION__ . ' requires second argument to be a named-associative array, but key '.$key.' is an integer.');
                 }
 
-                $query = preg_replace('/:'.$key.'(?![a-z])/', preg_quote($this->escapeAuto($value, $key)), $query);
+                $value = preg_replace('#(?<!\\\\)(\\$|\\\\)#', '\\\\$1', $this->escapeAuto($value, $key));
+                $query = preg_replace('/:'.$key.'(?![a-z])/', $value, $query);
             }
         }
         else
