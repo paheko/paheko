c904fb1590996f5c1de3bc1a7950f981712dff81
bohwaz <bohwaz@kd2.org>
2012-10-17 03:49:30 +0000

    Nouveaux graphs
    FossilOrigin-Name: 736a2f9b0ce79e00357491c449fa1e27fbf1084a

--- include/class.compta_stats.php
+++ include/class.compta_stats.php
@@ -23,6 +23,44 @@ class Garradin_Compta_Stats
 		return $this->_byType(1);
 	}
 
+	public function soldeCompte($compte, $augmente = 'debit', $diminue = 'credit')
+	{
+		$db = Garradin_DB::getInstance();
+
+		if (strpos($compte, '%') !== false)
+		{
+			$compte = 'LIKE \''. $db->escapeString($compte) . '\'';
+		}
+		else
+		{
+			$compte = '= \''. $db->escapeString($compte) . '\'';
+		}
+
+		$stats = $this->getStats('SELECT strftime(\'%Y%m\', date) AS date,
+			(COALESCE((SELECT SUM(montant) FROM compta_journal
+				WHERE compte_'.$augmente.' '.$compte.' AND id_exercice = cj.id_exercice
+				AND date >= strftime(\'%Y-%m-01\', cj.date)
+				AND date <= strftime(\'%Y-%m-31\', cj.date)), 0)
+			- COALESCE((SELECT SUM(montant) FROM compta_journal
+				WHERE compte_'.$diminue.' '.$compte.' AND id_exercice = cj.id_exercice
+				AND date >= strftime(\'%Y-%m-01\', cj.date)
+				AND date <= strftime(\'%Y-%m-31\', cj.date)), 0)
+			) AS solde
+			FROM compta_journal AS cj
+			WHERE (compte_debit '.$compte.' OR compte_credit '.$compte.')
+			AND id_exercice = (SELECT id FROM compta_exercices WHERE cloture = 0)
+			GROUP BY strftime(\'%Y-%m\', date) ORDER BY date;');
+
+		$c = 0;
+		foreach ($stats as $k=>$v)
+		{
+			$c += $v;
+			$stats[$k] = $c;
+		}
+
+		return $stats;
+	}
+
 	public function getStats($query)
 	{
 		$db = Garradin_DB::getInstance();
--- www/admin/compta/graph.php
+++ www/admin/compta/graph.php
@@ -30,16 +30,64 @@ if (utils::get('g') == 'recettes_depenses')
 
 	$plot->setLabels($labels);
 }
+elseif (utils::get('g') == 'banques_caisses')
+{
+	require_once GARRADIN_ROOT . '/include/class.compta_comptes_bancaires.php';
+	$banques = new Garradin_Compta_Comptes_Bancaires;
+
+	$data = array();
+
+	$r = new SVGPlot_Data($stats->soldeCompte(Garradin_Compta_Comptes::CAISSE));
+	$r->title = 'Caisse';
+
+	$data[] = $r;
+
+	foreach ($banques->getList() as $banque)
+	{
+		$r = new SVGPlot_Data($stats->soldeCompte($banque['id']));
+		$r->title = $banque['libelle'];
+		$data[] = $r;
+	}
+
+	$plot->setTitle('Solde des comptes et caisses');
+}
+elseif (utils::get('g') == 'dettes')
+{
+	$data = array();
+
+	$r = new SVGPlot_Data($stats->soldeCompte('401%', 'credit', 'debit'));
+	$r->title = 'Dettes fournisseurs';
+	$data[] = $r;
+
+	$r = new SVGPlot_Data($stats->soldeCompte('411%', 'credit', 'debit'));
+	$r->title = 'Dettes usagers';
+	$data[] = $r;
+
+	$plot->setTitle('Dettes');
+}
 
 if (!empty($data))
 {
+	$labels = array();
+
+	foreach ($data[0]->get() as $k=>$v)
+	{
+		$labels[] = utils::date_fr('M y', strtotime(substr($k, 0, 4) . '-' . substr($k, 4, 2) .'-01'));
+	}
+
+	$plot->setLabels($labels);
+
 	$i = 0;
+	$colors = array('#fa4', '#941', '#af4', '#4af', '#a4f');
 
 	foreach ($data as $line)
 	{
-		$line->color = ($i++ % 2) ? '#fa4' : '#9c4f15';
+		$line->color = $colors[$i++];
 		$line->width = 2;
 		$plot->add($line);
+
+		if ($i > count($colors))
+			$i = 0;
 	}
 }
 
