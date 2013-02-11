fe965b546fffaaa2851fdf246f8d0808f4822898
bohwaz <bohwaz@noemail.net>
2013-02-11 16:07:22 +0000

    Partie fonctionnelle de la gestion des sauvegardes
    FossilOrigin-Name: 3006216dd11947a968b48c9a252003b2b0c367b3

--- www/admin/config/donnees.php
+++ www/admin/config/donnees.php
@@ -4,12 +4,77 @@ namespace Garradin;
 require_once __DIR__ . '/_inc.php';
 
 $s = new Sauvegarde;
+$error = false;
 
-if (utils::get('sauvegarde'))
+if (utils::post('config'))
 {
+    if (!utils::CSRF_check('backup_config'))
+    {
+        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
+    }
+    else
+    {
+        $config->set('frequence_sauvegardes', utils::post('frequence_sauvegardes'));
+        $config->set('nombre_sauvegardes', utils::post('nombre_sauvegardes'));
+        $config->save();
 
+        utils::redirect('/admin/config/donnees.php?ok=config');
+    }
+}
+elseif (utils::post('create'))
+{
+    if (!utils::CSRF_check('backup_create'))
+    {
+        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
+    }
+    else
+    {
+        $s->create();
+        utils::redirect('/admin/config/donnees.php?ok=create');
+    }
+}
+elseif (utils::post('download'))
+{
+    if (!utils::CSRF_check('backup_download'))
+    {
+        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
+    }
+    else
+    {
+        header('Content-type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . $config->get('nom_asso') . ' - Sauvegarde données - ' . date('Y-m-d') . '.sqlite"');
+
+        $s->dump();
+        exit;
+    }
+}
+elseif (utils::post('restore'))
+{
+    if (!utils::CSRF_check('backup_manage'))
+    {
+        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
+    }
+    else
+    {
+        $s->restoreFromLocal(utils::post('file'));
+        utils::redirect('/admin/config/donnees.php?ok=restore');
+    }
+}
+elseif (utils::post('remove'))
+{
+    if (!utils::CSRF_check('backup_manage'))
+    {
+        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
+    }
+    else
+    {
+        $s->remove(utils::post('file'));
+        utils::redirect('/admin/config/donnees.php?ok=remove');
+    }
 }
 
+$tpl->assign('error', $error);
+$tpl->assign('ok', utils::get('ok'));
 $tpl->assign('liste', $s->getList());
 
 $tpl->display('admin/config/donnees.tpl');
