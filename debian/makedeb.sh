80d82bba117a5d0acd51a524922a888c26af3906
bohwaz <bohwaz@noemail.net>
2013-07-28 09:03:21 +0000

    Débuts de la version hors-ligne
    FossilOrigin-Name: fb49f7d43ec7212b18cdf3a8d21dcbba8989890d

---
+++ debian/makedeb.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+# Ripped from fossil makdedeb.sh
+
+DEB_REV=${1-1} # .deb package build/revision number.
+PACKAGE_DEBNAME=garradin
+THISDIR=${PWD}
+
+DEB_ARCH_NAME=all
+
+SRCDIR=$(cd ..; pwd)/src
+test -e ${SRCDIR} || {
+    echo "This script must be run from a BUILT copy of the source tree."
+    exit 1
+}
+
+DEBROOT=$PWD/deb.tmp
+test -d ${DEBROOT} && rm -fr ${DEBROOT}
+
+DEBLOCALPREFIX=${DEBROOT}/usr
+BINDIR=${DEBLOCALPREFIX}/bin
+mkdir -p ${BINDIR}
+mkdir -p ${DEBLOCALPREFIX}/share/doc/${PACKAGE_DEBNAME}
+cp ${THISDIR}/garradin ${BINDIR}
+
+CODEDIR=${DEBLOCALPREFIX}/share/${PACKAGE_DEBNAME}
+mkdir -p ${CODEDIR}
+cp -r ${SRCDIR}/* ${CODEDIR}
+
+# Cleaning files that will be copied to /usr/share/doc
+rm -f ${CODEDIR}/{README,COPYING}
+
+cd $DEBROOT || {
+    echo "Debian dest dir [$DEBROOT] not found. :("
+    exit 2
+}
+
+rm -fr DEBIAN
+mkdir DEBIAN
+
+PACKAGE_VERSION=`cat ${SRCDIR}/VERSION`
+PACKAGE_DEB_VERSION=${PACKAGE_VERSION}-${DEB_REV}
+DEBFILE=${THISDIR}/${PACKAGE_DEBNAME}-${PACKAGE_DEB_VERSION}-dev-${DEB_ARCH_NAME}.deb
+PACKAGE_TIME=$(/bin/date)
+
+rm -f ${DEBFILE}
+echo "Creating .deb package [${DEBFILE}]..."
+
+echo "Generating md5 sums..."
+find ${DEBLOCALPREFIX} -type f -exec md5sum {} \; > DEBIAN/md5sums
+
+true && {
+    echo "Generating Debian-specific files..."
+    cp ${SRCDIR}/COPYING ${DEBLOCALPREFIX}/share/doc/${PACKAGE_DEBNAME}/copyright
+} || {
+	echo "Fail."
+	exit 1
+}
+
+true && {
+    CHANGELOG=${DEBLOCALPREFIX}/share/doc/${PACKAGE_DEBNAME}/changelog.gz
+    cat <<EOF | gzip -c > ${CHANGELOG}
+${PACKAGE_DEBNAME} ${PACKAGE_DEB_VERSION}; urgency=low
+
+This release has no changes over the core source distribution. It has
+simply been Debianized.
+
+Packaged by ${USER} <http://dev.kd2.org/garradin/> on
+${PACKAGE_TIME}.
+
+EOF
+
+}
+
+# doc.
+cp ${SRCDIR}/README ${DEBLOCALPREFIX}/share/doc/${PACKAGE_DEBNAME}
+
+
+true && {
+    CONTROL=DEBIAN/control
+    echo "Generating ${CONTROL}..."
+    cat <<EOF > ${CONTROL}
+Package: ${PACKAGE_DEBNAME}
+Section: web
+Priority: optional
+Maintainer: Garradin <garradin@kd2.org>
+Architecture: ${DEB_ARCH_NAME}
+Depends: dash | bash, php5-cli (>=5.4)
+Version: ${PACKAGE_DEB_VERSION}
+Suggests: www-browser
+Homepage: http://garradin.kd2.org/
+Description: Garradin is a tool to manage non-profit organizations.
+ It's only available in french.
+Description-fr: Gestionnaire d'association en interface web ou CLI.
+ Garradin est un gestionnaire d'association à but non lucratif.
+ Il permet de gérer les membres, leur adhésion et leurs contributions financières.
+ Les membres peuvent se connecter eux-même et modifier leurs informations
+ ou communiquer entre eux par e-mail. Une gestion précise des droits et
+ autorisations est possible. Il est également possible de faire des
+ envois de mails en groupe.
+ .
+ Un module de comptabilité à double entrée assure une gestion financière
+ complète digne d'un vrai logiciel de comptabilité : suivi des opérations,
+ graphiques, bilan annuel, compte de résultat, exercices, etc.
+ .
+ Un module wiki permet de prendre des notes de réunion, tenir à jour
+ les informations internes à l'association (possibilité de chiffrer le
+ contenu des pages) ou de publier des pages sur le site public intégré.
+ L'aspect du site public peut être géré simplement avec ses squelettes
+ SPIP.
+
+EOF
+
+}
+
+
+true && {
+    dpkg-deb -b ${DEBROOT} ${DEBFILE}
+    echo "Package file created:"
+    ls -la ${DEBFILE}
+    dpkg-deb --info ${DEBFILE}
+}
+
+cd - >/dev/null
+true && {
+    echo "Cleaning up..."
+    rm -fr ${DEBROOT}
+}
+
+echo "Done :)"
