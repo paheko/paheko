ea08d4e6ac992ecf8d6ea0aa83cfe7da6fbe0276
bohwaz <bohwaz@kd2.org>
2013-09-02 08:48:58 +0000

    Arrêtons d'essayer d'être intelligents, dans 90% des cas ça merde
    FossilOrigin-Name: eaeee5f5c5cacb01b7b533a05c473a981e202c4e

--- src/index.php
+++ src/index.php
@@ -1,90 +1,23 @@
 <?php
 
-if (!version_compare(phpversion(), '5.3.0', '>='))
+// Ce fichier n'est pas censé être appelé sauf si l'installation de Garradin
+// n'est pas effectuée correctement avec le vhost pointant sur le répertoire www/
+// auquel cas on limite les dégâts
+
+// Juste une vérification avant de continuer
+if (!version_compare(phpversion(), '5.4.0', '>='))
 {
-	die('PHP 5.3.0 ou supérieur est nécessaire au fonctionnement de Garradin.');
+	die('PHP 5.4.0 ou supérieur est nécessaire au fonctionnement de Garradin.');
 }
 
-define('GARRADIN_INSTALL_PROCESS', true);
-require __DIR__ . '/include/init.php';
-
-if (!defined('PHP_SAPI') || file_exists(GARRADIN_ROOT . '/.garradinRootProcessed'))
+if (file_exists(__DIR__ . '/.garradinRootProcessed'))
 {
-	header('Location: '.WWW_URL);
+	header('Location: www/');
 	exit;
 }
 
-if (preg_match('/^apache/', PHP_SAPI))
-{
-	// On tente de faire un .htaccess qui redirige la racine vers /www/
-	// de manière transparente si Garradin n'est pas configuré
-	// pour que la racine soit déjà dans www/
-	if (!file_exists(__DIR__ . '/.htaccess'))
-	{
-		file_put_contents(__DIR__ . '/.htaccess',
-			"RewriteEngine On\n" .
-			"RewriteCond %{REQUEST_URI} !^".WWW_URI."\n" .
-			"RewriteRule ^(.*)$ ".WWW_URI."$1 [QSA,L]\n");
-	}
-
-	$uri = dirname(WWW_URI);
-
-	if (substr($uri, -1) != '/')
-	{
-		$uri .= '/';
-	}
-
-	$url = 'http' . (!empty($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $uri;
-
-	$headers = get_headers($url . 'admin/install.php');
-
-	// On regarde si ça redirige bien
-	if (!preg_match('!^HTTP/[0-9.]+ 200!', $headers[0]))
-	{
-		// Sinon on tente de faire une simple redirection
-		file_put_contents(__DIR__ . '/.htaccess',
-			"RedirectMatch ".dirname(WWW_URI)."/((?!www).*) ".WWW_URI."$1\n");
-
-		$headers = get_headers($url . 'admin/install.php');
-
-		// Si ça ne marche toujours pas tant pis
-		if (!preg_match('!^HTTP/[0-9.]+ 302!', $headers[0]))
-		{
-			unlink(__DIR__ . '/.htaccess');
-		}
-		else
-		{
-			// On vérifie quand même que ça ne part pas en redirection infinie
-			// normalement c'est impossible mais on sait jamais
-			$headers = get_headers($url . 'www/admin/install.php');
-
-			// Redirection infinie ou erreur 500 sur /www/ -> abandon
-			if (!preg_match('!^HTTP/[0-9.]+ 404!', $headers[0]))
-			{
-				unlink(__DIR__ . '/.htaccess');
-			}
-			else
-			{
-				// C'est bon pour la redir !
-				touch(GARRADIN_ROOT . '/.garradinRootProcessed');
-				header('Location: '.WWW_URL);
-				exit;
-			}
-		}
-	}
-	else
-	{
-		// C'est bon pour le rewrite !
-		touch(GARRADIN_ROOT . '/.garradinRootProcessed');
-		file_put_contents(GARRADIN_ROOT . '/config.local.php', '<?php define(\'WWW_URI\', \''.$uri.'\'); ?>');
-		header('Location: '.$url);
-		exit;
-	}
-}
-
-// Si serveur non Apache, ou ni RewriteRule ni RedirectMatch ne fonctionnent,
-// on empêche quand même de lister les répertoires qui ne sont pas censés être publics
-$dir = dir(GARRADIN_ROOT);
+// on empêche de lister les répertoires qui ne sont pas censés être publics
+$dir = dir(__DIR__);
 
 while ($file = $dir->read())
 {
@@ -93,12 +26,12 @@ while ($file = $dir->read())
 		continue;
 	}
 
-	if (!is_dir(GARRADIN_ROOT . '/' . $file) || $file == 'www')
+	if (!is_dir(__DIR__ . '/' . $file) || $file == 'www')
 	{
 		continue;
 	}
 
-	file_put_contents(GARRADIN_ROOT . '/' . $file . '/index.html',
+	file_put_contents(__DIR__ . '/' . $file . '/index.html',
 		'<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">' .
 		'<html><head><title>404 Not Found</title></head><body>' .
 		'<h1>Not Found</h1><p>The requested URL was not found on this server.</p>' .
@@ -108,6 +41,6 @@ while ($file = $dir->read())
 $dir->close();
 
 touch(GARRADIN_ROOT . '/.garradinRootProcessed');
-header('Location: '.WWW_URL);
+header('Location: www/');
 
 ?>
