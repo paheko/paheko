1f60a30e9d9463c5e1685dcec50b1800efde3c24
bohwaz <bohwaz@kd2.org>
2013-09-18 14:14:16 +0000

    Correction "mes infos" : le javascript pour input type=date n'était pas chargé
    FossilOrigin-Name: 07b472ca6200357e422caf5ebabf2964283f46e4

--- src/www/admin/mes_infos.php
+++ src/www/admin/mes_infos.php
@@ -50,6 +50,8 @@ $tpl->assign('champs', $config->get('champs_membres')->getAll());
 
 $tpl->assign('membre', $membre);
 
+$tpl->assign('custom_js', array('datepickr.js'));
+
 $tpl->display('admin/mes_infos.tpl');
 
 ?>
