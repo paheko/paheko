b4bdc43a80b96c4db61dc080c5510423c366ab9f
bohwaz <bohwaz@noemail.net>
2014-02-06 18:59:06 +0000

    Liste des écritures réalisées par un membre
    FossilOrigin-Name: 4d6e4631551ff87c9e0c9b0fc115cca010964ff3

---
+++ src/www/admin/compta/operations/membre.php
@@ -0,0 +1,51 @@
+<?php
+namespace Garradin;
+
+require_once __DIR__ . '/../_inc.php';
+
+$exercices = new Compta_Exercices;
+$journal = new Compta_Journal;
+
+$exercice = utils::get('exercice') ?: $exercices->getCurrentId();
+
+if (!$exercice)
+{
+	throw new UserException('Exercice inconnu.');
+}
+
+if (empty($_GET['id']) || !is_numeric($_GET['id']))
+{
+    throw new UserException("Argument du numéro de membre manquant.");
+}
+
+$id = (int) $_GET['id'];
+
+$membre = $membres->get($id);
+
+if (!$membre)
+{
+    throw new UserException("Le membre demandé n'existe pas.");
+}
+
+$liste_comptes = $comptes->getListAll();
+
+function get_nom_compte($compte)
+{
+	if (is_null($compte))
+		return '';
+
+	global $liste_comptes;
+	return $liste_comptes[$compte];
+}
+
+$tpl->register_modifier('get_nom_compte', 'Garradin\get_nom_compte');
+
+$tpl->assign('journal', $journal->listForMember($membre['id'], $exercice));
+
+$tpl->assign('exercices', $exercices->getList());
+$tpl->assign('exercice', $exercice);
+$tpl->assign('membre', $membre);
+
+$tpl->display('admin/compta/operations/membre.tpl');
+
+?>
