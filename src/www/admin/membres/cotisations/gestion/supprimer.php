b69f6f4506cd7cc2f4ec5c21dc14cb150f8411ee
bohwaz <bohwaz@noemail.net>
2014-02-13 17:25:12 +0000

    Suppression de cotisation
    FossilOrigin-Name: 1fa74feed84fa74c8b7135a1c81376652ac08a8b

---
+++ src/www/admin/membres/cotisations/gestion/supprimer.php
@@ -0,0 +1,47 @@
+<?php
+namespace Garradin;
+
+require_once __DIR__ . '/../../../_inc.php';
+
+if ($user['droits']['membres'] < Membres::DROIT_ADMIN)
+{
+    throw new UserException("Vous n'avez pas le droit d'accéder à cette page.");
+}
+
+$cotisations = new Cotisations;
+
+$co = $cotisations->get(utils::get('id'));
+
+if (!$co)
+{
+    throw new UserException("Cette cotisation n'existe pas.");
+}
+
+$error = false;
+
+if (!empty($_POST['delete']))
+{
+    if (!utils::CSRF_check('delete_co_' . $co['id']))
+    {
+        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
+    }
+    else
+    {
+        try {
+            $cotisations->delete($co['id']);
+            utils::redirect('/admin/membres/cotisations/');
+        }
+        catch (UserException $e)
+        {
+            $error = $e->getMessage();
+        }
+    }
+}
+
+$tpl->assign('error', $error);
+
+$tpl->assign('cotisation', $co);
+
+$tpl->display('admin/membres/cotisations/gestion/supprimer.tpl');
+
+?>
