0aa7bc30a598f288bea1069bdddbcfdb7ae0c7c8
bohwaz <bohwaz@kd2.org>
2014-02-17 17:35:47 +0000

    État des cotisations pour le membre connecté
    FossilOrigin-Name: aa80b872002e49d1caa71202550a46863ad80146

---
+++ src/www/admin/mes_cotisations.php
@@ -0,0 +1,39 @@
+<?php
+namespace Garradin;
+
+require_once __DIR__ . '/_inc.php';
+
+$membre = $membres->getLoggedUser();
+
+if (!$membre)
+{
+    throw new UserException("Ce membre n'existe pas.");
+}
+
+$error = false;
+
+$tpl->assign('membre', $membre);
+
+$cats = new Membres_Categories;
+
+$categorie = $cats->get($membre['id_categorie']);
+$tpl->assign('categorie', $categorie);
+
+$cotisations = new Cotisations_Membres;
+
+if (!empty($categorie['id_cotisation_obligatoire']))
+{
+    $tpl->assign('cotisation', $cotisations->isMemberUpToDate($membre['id'], $categorie['id_cotisation_obligatoire']));
+}
+else
+{
+    $tpl->assign('cotisation', false);
+}
+
+$tpl->assign('nb_activites', $cotisations->countForMember($membre['id']));
+$tpl->assign('cotisations', $cotisations->listForMember($membre['id']));
+$tpl->assign('cotisations_membre', $cotisations->listSubscriptionsForMember($membre['id']));
+
+$tpl->display('admin/mes_cotisations.tpl');
+
+?>
