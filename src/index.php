73ec1722aa64b6f8f5137da6c6035056bf5d6595
bohwaz <bohwaz@kd2.org>
2014-03-31 13:02:54 +0000

    Les fichiers index.html sont fournis par défaut dans l'archive de Garradin, pas besoin de ce code
    FossilOrigin-Name: 2a855676822e1e38e197ebc9d58e48e4a1e7809e

--- src/index.php
+++ src/index.php
@@ -10,37 +10,4 @@ if (!version_compare(phpversion(), '5.4.0', '>='))
 	die('PHP 5.4.0 ou supérieur est nécessaire au fonctionnement de Garradin.');
 }
 
-if (file_exists(__DIR__ . '/.garradinRootProcessed'))
-{
-	header('Location: www/');
-	exit;
-}
-
-// on empêche de lister les répertoires qui ne sont pas censés être publics
-$dir = dir(__DIR__);
-
-while ($file = $dir->read())
-{
-	if ($file[0] == '.')
-	{
-		continue;
-	}
-
-	if (!is_dir(__DIR__ . '/' . $file) || $file == 'www')
-	{
-		continue;
-	}
-
-	file_put_contents(__DIR__ . '/' . $file . '/index.html',
-		'<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">' .
-		'<html><head><title>404 Not Found</title></head><body>' .
-		'<h1>Not Found</h1><p>The requested URL was not found on this server.</p>' .
-		'</body></html>');
-}
-
-$dir->close();
-
-touch(__DIR__ . '/.garradinRootProcessed');
 header('Location: www/');
-
-?>
