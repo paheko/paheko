aa6ccacd1d628c5ea57e0ac503a94b6ed5fcc3b7
bohwaz <bohwaz@noemail.net>
2015-02-04 01:48:11 +0000

    possibilité d'indiquer si un fichier est une image, ça simplifiera les galeries dans le wiki
    FossilOrigin-Name: 6ea3fe6cbb332a5c547159cad05d0ad5ef325ab7

--- src/include/data/0.7.0.sql
+++ src/include/data/0.7.0.sql
@@ -10,13 +10,14 @@ CREATE TABLE plugins_signaux
 CREATE TABLE fichiers
 -- Données sur les fichiers
 (
-	id INTEGER NOT NULL PRIMARY KEY,
-	nom TEXT NOT NULL, -- nom de fichier (par exemple image1234.jpeg)
-	type TEXT NOT NULL, -- Type MIME
-	titre TEXT NOT NULL, -- Titre/description
-	date TEXT NOT NULL DEFAULT CURRENT_DATE, -- Date d'ajout ou mise à jour du fichier
-	hash TEXT NOT NULL, -- Hash SHA1 du contenu du fichier
-	taille INTEGER NOT NULL -- Taille en octets
+    id INTEGER NOT NULL PRIMARY KEY,
+    nom TEXT NOT NULL, -- nom de fichier (par exemple image1234.jpeg)
+    type TEXT NOT NULL, -- Type MIME
+    image INTEGER NOT NULL DEFAULT 0, -- 1 = image reconnue
+    titre TEXT NOT NULL, -- Titre/description
+    date TEXT NOT NULL DEFAULT CURRENT_DATE, -- Date d'ajout ou mise à jour du fichier
+    hash TEXT NOT NULL, -- Hash SHA1 du contenu du fichier
+    taille INTEGER NOT NULL -- Taille en octets
 );
 
 CREATE UNIQUE INDEX fichiers_hash ON fichiers (hash);
@@ -26,27 +27,30 @@ CREATE INDEX fichiers_date ON fichiers (date);
 CREATE TABLE fichiers_contenu
 -- Contenu des fichiers
 (
-	id INTEGER NOT NULL PRIMARY KEY REFERENCES fichiers (id),
-	contenu BLOB
+    id INTEGER NOT NULL PRIMARY KEY REFERENCES fichiers (id),
+    contenu BLOB
 );
 
 CREATE TABLE fichiers_membres
 -- Associations entre fichiers et membres (photo de profil par exemple)
 (
-	fichier INTEGER NOT NULL REFERENCES fichiers (id),
-	id INTEGER NOT NULL REFERENCES membres (id)
+    fichier INTEGER NOT NULL REFERENCES fichiers (id),
+    id INTEGER NOT NULL REFERENCES membres (id),
+    PRIMARY KEY(fichier, id)
 );
 
 CREATE TABLE fichiers_wiki_pages
 -- Associations entre fichiers et pages du wiki
 (
-	fichier INTEGER NOT NULL REFERENCES fichiers (id),
-	id INTEGER NOT NULL REFERENCES wiki_pages (id)
+    fichier INTEGER NOT NULL REFERENCES fichiers (id),
+    id INTEGER NOT NULL REFERENCES wiki_pages (id),
+    PRIMARY KEY(fichier, id)
 );
 
 CREATE TABLE fichiers_compta_journal
 -- Associations entre fichiers et journal de compta (pièce comptable par exemple)
 (
-	fichier INTEGER NOT NULL REFERENCES fichiers (id),
-	id INTEGER NOT NULL REFERENCES compta_journal (id)
+    fichier INTEGER NOT NULL REFERENCES fichiers (id),
+    id INTEGER NOT NULL REFERENCES compta_journal (id),
+    PRIMARY KEY(fichier, id)
 );
--- src/include/data/schema.sql
+++ src/include/data/schema.sql
@@ -324,13 +324,13 @@ CREATE TABLE plugins_signaux
     PRIMARY KEY (signal, plugin)
 );
 
-
 CREATE TABLE fichiers
 -- Données sur les fichiers
 (
     id INTEGER NOT NULL PRIMARY KEY,
     nom TEXT NOT NULL, -- nom de fichier (par exemple image1234.jpeg)
     type TEXT NOT NULL, -- Type MIME
+    image INTEGER NOT NULL DEFAULT 0, -- 1 = image reconnue
     titre TEXT NOT NULL, -- Titre/description
     date TEXT NOT NULL DEFAULT CURRENT_DATE, -- Date d'ajout ou mise à jour du fichier
     hash TEXT NOT NULL, -- Hash SHA1 du contenu du fichier
@@ -352,19 +352,22 @@ CREATE TABLE fichiers_membres
 -- Associations entre fichiers et membres (photo de profil par exemple)
 (
     fichier INTEGER NOT NULL REFERENCES fichiers (id),
-    id INTEGER NOT NULL REFERENCES membres (id)
+    id INTEGER NOT NULL REFERENCES membres (id),
+    PRIMARY KEY(fichier, id)
 );
 
 CREATE TABLE fichiers_wiki_pages
 -- Associations entre fichiers et pages du wiki
 (
     fichier INTEGER NOT NULL REFERENCES fichiers (id),
-    id INTEGER NOT NULL REFERENCES wiki_pages (id)
+    id INTEGER NOT NULL REFERENCES wiki_pages (id),
+    PRIMARY KEY(fichier, id)
 );
 
 CREATE TABLE fichiers_compta_journal
 -- Associations entre fichiers et journal de compta (pièce comptable par exemple)
 (
     fichier INTEGER NOT NULL REFERENCES fichiers (id),
-    id INTEGER NOT NULL REFERENCES compta_journal (id)
+    id INTEGER NOT NULL REFERENCES compta_journal (id),
+    PRIMARY KEY(fichier, id)
 );
