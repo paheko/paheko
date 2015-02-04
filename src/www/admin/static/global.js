14241c173798366527f97a36e09292d5df51240f
bohwaz <bohwaz@noemail.net>
2015-02-04 05:28:24 +0000

    Unification/simplification du Javascript
    FossilOrigin-Name: 9c55662f073fbd88be899940adae0b731a95439c

--- src/www/admin/static/global.js
+++ src/www/admin/static/global.js
@@ -1,4 +1,10 @@
 (function () {
+    window.garradin = {
+    	url: window.location.href.replace(/\/admin\/.*?$/, ''),
+    	admin_url: window.location.href.replace(/\/admin\/.*?$/, '/admin/'),
+    	static_url: window.location.href.replace(/\/admin\/.*?$/, '/admin/static/')
+    };
+
 	window.$ = function(selector) {
 		if (!selector.match(/^[.#]?[a-z0-9_-]+$/i))
 		{
@@ -18,7 +24,21 @@
 		}
 	};
 
-    window.toggleElementVisibility = function(selector, visibility)
+	garradin.onload = function(callback, dom = true)
+    {
+    	var eventName = dom ? 'DOMContentLoaded' : 'load';
+
+		if (document.addEventListener)
+		{
+			document.addEventListener(eventName, callback, false);
+		}
+		else
+		{
+			document.attachEvent('on' + eventName, callback);
+		}
+    };
+
+    window.garradin.toggleElementVisibility = function(selector, visibility)
     {
     	if (!('classList' in document.documentElement))
     		return false;
@@ -46,7 +66,25 @@
         return true;
     };
 
-	function dateInputFallback()
+    garradin.script = function (file) {
+    	var script = document.createElement('script');
+    	script.type = 'text/javascript';
+    	script.src = this.static_url + file;
+    	return document.head.appendChild(script);
+    };
+
+    garradin.style = function (file) {
+    	var link = document.createElement('link');
+    	link.rel = 'stylesheet';
+    	link.type = 'text/css';
+    	link.href = this.static_url + file;
+    	return document.head.appendChild(link);
+    };
+
+    // From KD2fw/js/xhr.js
+	garradin.load = function(b,d,f,e){var a=new XMLHttpRequest();if(!a||!b)return false;if(a.overrideMimeType)a.overrideMimeType('text/xml');b+=(b.indexOf('?')+1?'&':'?')+(+(new Date));a.onreadystatechange=function(){if(a.readyState!=4)return;if((s=a.status)==200){if(!d)return true;var c=a.responseText;if(f=='json'){return((j=window.JSON)&&j.parse)?j.parse(c):eval('('+c.replace(/[\n\r]/g,'')+')')}d(c)}else if(e){e(s)}};a.open('GET',b,true);a.send(null)};
+
+	var dateInputFallback = function ()
 	{
 		var input = document.createElement('input');
 		input.setAttribute('type', 'date');
@@ -59,29 +97,19 @@
 		// the input type date element is not supported
 		if (input.type === 'text' || input.value === ':-)')
 		{
-			var www_url = document.body.getAttribute('data-url') + 'static/';
+			document.body.removeChild(input);
 
-			var script = document.createElement('script');
-			script.type = "text/javascript";
-			script.src = www_url + 'datepickr.js';
-			document.head.appendChild(script);
-			
-			var link = document.createElement('link');
-			link.type = 'text/css';
-			link.rel = 'stylesheet';
-			link.href = www_url + 'datepickr.css';
-			document.head.appendChild(link);
+			if (document.querySelector && !document.querySelector('input[type=date]'))
+				return false;
+
+			garradin.script('scripts/datepickr.js');
+			garradin.style('scripts/datepickr.css');
 		}
+		else
+		{
+			document.body.removeChild(input);
+		}
+	};
 
-		document.body.removeChild(input);
-	}
-
-	if (document.addEventListener)
-	{
-		document.addEventListener("DOMContentLoaded", dateInputFallback, false);
-	}
-	else
-	{
-		document.attachEvent("onDOMContentLoaded", dateInputFallback);
-	}
+	garradin.onload(dateInputFallback);
 })();
