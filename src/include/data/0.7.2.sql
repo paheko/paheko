ea1b60cdc5eaf490fd7b36b0b52cde54c36785ef
bohwaz <bohwaz@kd2.org>
2016-05-31 03:21:16 +0000

    Ne pas permettre de faire des ID de compte en minuscule
    FossilOrigin-Name: 2ff14ece2ad55eea335ddfe7edfc1e8bfdf56394

--- src/include/data/0.7.2.sql
+++ src/include/data/0.7.2.sql
@@ -1,1 +1,6 @@
+# Colonne manquante
 ALTER TABLE rappels_envoyes ADD COLUMN id_rappel INTEGER NULL REFERENCES rappels (id);
+
+# Un bug a permis d'insérer des comptes avec des lettres minuscules, créant des problèmes
+# corrigeons donc les comptes pour les mettre en majuscules.
+UPDATE compta_comptes SET id = UPPER(id);
