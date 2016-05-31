63cd8f39294cbfbb47fc47b4ed679bbce1aeff1f
bohwaz <bohwaz@noemail.net>
2016-05-31 03:40:33 +0000

    Quand on supprime une cotisation on veut aussi supprimer les rappels liés.
    FossilOrigin-Name: 2404bb932f1b3113ad2971d74de78509ddf672b5

--- src/include/lib/Garradin/Cotisations.php
+++ src/include/lib/Garradin/Cotisations.php
@@ -123,6 +123,9 @@ class Cotisations
 		$db->simpleExec('UPDATE membres_operations SET id_cotisation = NULL 
 			WHERE id_cotisation IN (SELECT id FROM cotisations_membres WHERE id_cotisation = ?);', (int) $id);
 
+		$db->simpleExec('DELETE FROM rappels WHERE id_cotisation = ?;', (int) $id);
+		$db->simpleExec('DELETE FROM rappels_envoyes WHERE id_cotisation = ?;', (int) $id);
+
 		$db->simpleExec('DELETE FROM cotisations_membres WHERE id_cotisation = ?;', (int) $id);
 		$db->simpleExec('DELETE FROM cotisations WHERE id = ?;', (int) $id);
 		$db->exec('END;');
