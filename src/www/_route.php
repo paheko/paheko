7e1e79530f9a1a6c79031531a72ed377c4b3f836
bohwaz <bohwaz@kd2.org>
2017-01-16 20:38:40 +0000

    Fix plugin name match in router
    FossilOrigin-Name: 5004acdf3a4e94cbdaa8ab2a26f2e6a9f423db17

--- src/www/_route.php
+++ src/www/_route.php
@@ -18,7 +18,7 @@ if (file_exists(__DIR__ . $uri))
 {
 	return false;
 }
-elseif (preg_match('!/admin/plugin/(.+)/(.*)!', $uri, $match))
+elseif (preg_match('!/admin/plugin/(.+?)/(.*)!', $uri, $match))
 {
 	$_GET['_p'] = $match[1];
 	$_GET['_u'] = $match[2];
