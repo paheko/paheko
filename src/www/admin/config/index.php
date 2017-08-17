cb23ec501495fe22c692bd1b5b0a3921e098bfc9
bohwaz <bohwaz@kd2.org>
2017-08-17 02:45:55 +0000

    Correction rapport d'erreur dans page config
    FossilOrigin-Name: 06ebefa8c797c1141c63e1ba18f5f4abd2232c45

--- src/www/admin/config/index.php
+++ src/www/admin/config/index.php
@@ -6,52 +6,45 @@ require_once __DIR__ . '/_inc.php';
 $couleur1 = '#9c4f15';
 $couleur2 = '#d98628';
 
-if (f('save'))
+if (f('save') && $form->check('config'))
 {
-    if (!Utils::CSRF_check('config'))
-    {
-        $error = 'Une erreur est survenue, merci de renvoyer le formulaire.';
-    }
-    else
-    {
-        try {
-            $config->set('nom_asso', f('nom_asso'));
-            $config->set('email_asso', f('email_asso'));
-            $config->set('adresse_asso', f('adresse_asso'));
-            $config->set('site_asso', f('site_asso'));
-            $config->set('email_envoi_automatique', f('email_envoi_automatique'));
-            $config->set('accueil_wiki', f('accueil_wiki'));
-            $config->set('accueil_connexion', f('accueil_connexion'));
-            $config->set('categorie_membres', f('categorie_membres'));
-            
-            $config->set('champ_identite', f('champ_identite'));
-            $config->set('champ_identifiant', f('champ_identifiant'));
+    try {
+        $config->set('nom_asso', f('nom_asso'));
+        $config->set('email_asso', f('email_asso'));
+        $config->set('adresse_asso', f('adresse_asso'));
+        $config->set('site_asso', f('site_asso'));
+        $config->set('email_envoi_automatique', f('email_envoi_automatique'));
+        $config->set('accueil_wiki', f('accueil_wiki'));
+        $config->set('accueil_connexion', f('accueil_connexion'));
+        $config->set('categorie_membres', f('categorie_membres'));
+        
+        $config->set('champ_identite', f('champ_identite'));
+        $config->set('champ_identifiant', f('champ_identifiant'));
 
-            $config->set('pays', f('pays'));
-            $config->set('monnaie', f('monnaie'));
+        $config->set('pays', f('pays'));
+        $config->set('monnaie', f('monnaie'));
 
-            // N'enregistrer les couleurs que si ce ne sont pas les couleurs par défaut
-            if (f('couleur1') != $couleur1 || f('couleur2') != $couleur2)
-            {
-                $config->set('couleur1', f('couleur1'));
-                $config->set('couleur2', f('couleur2'));
-                $config->set('image_fond', f('image_fond'));
-            }
-            else
-            {
-                $config->set('couleur1', null);
-                $config->set('couleur2', null);
-                $config->set('image_fond', null);
-            }
-
-            $config->save();
-
-            Utils::redirect('/admin/config/?ok');
-        }
-        catch (UserException $e)
+        // N'enregistrer les couleurs que si ce ne sont pas les couleurs par défaut
+        if (f('couleur1') != $couleur1 || f('couleur2') != $couleur2)
         {
-            $error = $e->getMessage();
+            $config->set('couleur1', f('couleur1'));
+            $config->set('couleur2', f('couleur2'));
+            $config->set('image_fond', f('image_fond'));
         }
+        else
+        {
+            $config->set('couleur1', null);
+            $config->set('couleur2', null);
+            $config->set('image_fond', null);
+        }
+
+        $config->save();
+
+        Utils::redirect('/admin/config/?ok');
+    }
+    catch (UserException $e)
+    {
+        $form->addError($e->getMessage());
     }
 }
 
