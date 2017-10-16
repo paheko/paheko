8b85f0575c0d0d8c24553d8c7c2c8ebe6cedb911
bohwaz <bohwaz@kd2.org>
2017-10-16 21:04:09 +0000

    Ajout liens vers comptes d'encaissement
    FossilOrigin-Name: d3c27b7bab12f5d1db39fdc089f72c5a279f4655

--- src/www/admin/compta/_inc.php
+++ src/www/admin/compta/_inc.php
@@ -9,3 +9,5 @@ $session->requireAccess('compta', Membres::DROIT_ACCES);
 $comptes = new Compta\Comptes;
 
 $tpl->assign('id_caisse', Compta\Comptes::CAISSE);
+$tpl->assign('id_cheque_a_encaisser', Compta\Comptes::CHEQUE_A_ENCAISSER);
+$tpl->assign('id_carte_a_encaisser', Compta\Comptes::CARTE_A_ENCAISSER);
