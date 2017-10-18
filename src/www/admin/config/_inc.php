b9f8e70fa4f8efc8f3907707078355b9a3dab9e4
bohwaz <bohwaz@noemail.net>
2017-10-18 08:57:57 +0000

    Nécessite le droit config, pas membres
    FossilOrigin-Name: fe67ead08e1032bc69c0188d4c763675b17fe8df

--- src/www/admin/config/_inc.php
+++ src/www/admin/config/_inc.php
@@ -4,6 +4,6 @@ namespace Garradin;
 
 require_once __DIR__ . '/../_inc.php';
 
-$session->requireAccess('membres', Membres::DROIT_ADMIN);
+$session->requireAccess('config', Membres::DROIT_ADMIN);
 
 $tpl->assign('garradin_website', WEBSITE);
