1e6c536c31e4c04bf97bb318a8c5b4f9a4bb817f
bohwaz <bohwaz@kd2.org>
2018-08-10 18:37:26 +0000

    Permettre de désactiver la fonction site web public
    FossilOrigin-Name: 2d9f29119407922b9355d7ae8b10d378c1c258be

--- src/www/index.php
+++ src/www/index.php
@@ -4,5 +4,10 @@ namespace Garradin;
 
 require __DIR__ . '/_inc.php';
 
+if (Config::getInstance()->get('desactiver_site'))
+{
+	Utils::redirect(ADMIN_URL);
+}
+
 $squelette = new Squelette;
 $squelette->dispatchURI();
