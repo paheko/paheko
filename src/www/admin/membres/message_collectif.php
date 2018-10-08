4ac05747e4f3c395e7a91bc00754f2e8162cb755
bohwaz <bohwaz@kd2.org>
2018-10-08 11:01:30 +0000

    Envoi de message collectif
    FossilOrigin-Name: e77b5d23b2748760f2cacbe0eb7bedeefa3e410b

---
+++ src/www/admin/membres/message_collectif.php
@@ -0,0 +1,56 @@
+<?php
+namespace Garradin;
+
+require_once __DIR__ . '/_inc.php';
+
+$cats = new Membres\Categories;
+$recherche = new Recherche;
+
+if (f('send'))
+{
+    $form->check('send_message_co', [
+        'sujet'      => 'required|string',
+        'message'    => 'required|string',
+        'recipients' => 'required|string',
+    ]);
+
+    if (preg_match('/^(categorie|recherche)_(\d+)$/', f('recipients'), $match))
+    {
+        if ($match[1] == 'categorie')
+        {
+            $recipients = $membres->listAllByCategory($match[2]);
+        }
+        else
+        {
+            $recipients = $recherche->search($match[2], 'id, email');
+        }
+
+        if (!count($recipients) || !isset($recipients[0]->email))
+        {
+            $form->addError('Aucun membre dans la liste.');
+        }
+    }
+    else
+    {
+        throw new UserException('Destinataires invalides : ' . f('recipients'));
+    }
+
+    if (!$form->hasErrors())
+    {
+        try {
+            $membres->sendMessage($recipients, f('sujet'),
+                f('message'), (bool) f('copie'));
+
+            Utils::redirect(ADMIN_URL . 'membres/?sent');
+        }
+        catch (UserException $e)
+        {
+            $form->addError($e->getMessage());
+        }
+    }
+}
+
+$tpl->assign('categories', $cats->listNotHidden());
+$tpl->assign('recherches', $recherche->getList($user->id, 'membres'));
+
+$tpl->display('admin/membres/message_collectif.tpl');
