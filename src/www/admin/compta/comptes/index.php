84fba12ff2a2e55f86f26c1797c4d20ef62c1fdd
bohwaz <bohwaz@kd2.org>
2019-05-26 11:40:57 +0000

    Import / export de plan comptable au format JSON, à partir du patch envoyé par @adresse
    FossilOrigin-Name: 9f18af1dbe2e1db17b2fd6ea9880a7ba87358290

--- src/www/admin/compta/comptes/index.php
+++ src/www/admin/compta/comptes/index.php
@@ -5,28 +5,67 @@ require_once __DIR__ . '/../_inc.php';
 
 $session->requireAccess('compta', Membres::DROIT_ADMIN);
 
+if (qg('export') == 'plan')
+{
+	$comptes->exportPlan();
+	exit;
+}
+
+$tpl->assign('confirm', qg('confirm'));
+
+if (f('import') && $form->check('plan_import', ['upload' => 'file|required', 'format' => 'required|in:json']))
+{
+	try {
+		$comptes->importPlan($_FILES['upload']['tmp_name'], true);
+		Utils::redirect(ADMIN_URL . 'compta/comptes/?import&confirm=import');
+	}
+	catch (UserException $e) {
+		$form->addError($e->getMessage());
+	}
+}
+elseif (f('reset') && $form->check('plan_reset'))
+{
+	try {
+		$comptes->importPlan(null, true);
+		Utils::redirect(ADMIN_URL . 'compta/comptes/?import&confirm=reset');
+	}
+	catch (UserException $e) {
+		$form->addError($e->getMessage());
+	}
+}
+
 $classe = (int) qg('classe');
 
 $tpl->assign('classe', $classe);
 
 if (!$classe)
 {
-    $tpl->assign('classes', $comptes->listTree(0, false));
+	$tpl->assign('classes', $comptes->listTree(0, false));
 }
 else
 {
-    $positions = $comptes->getPositions();
+	$positions = $comptes->getPositions();
 
-    $tpl->assign('classe_compte', $comptes->get($classe));
-    $tpl->assign('liste', $comptes->listTree($classe));
+	$tpl->assign('classe_compte', $comptes->get($classe));
+	$tpl->assign('liste', $comptes->listTree($classe));
 }
 
 function tpl_get_position($pos)
 {
-    global $positions;
-    return $positions[$pos];
+	global $positions;
+	return $positions[$pos];
 }
 
 $tpl->register_modifier('get_position', 'Garradin\tpl_get_position');
 
-$tpl->display('admin/compta/comptes/index.tpl');
+$template = 'index';
+
+if ($classe) {
+	$template = 'classe';
+}
+elseif (qg('import') !== null) {
+	$template = 'import';
+	$tpl->assign('confirm', qg('confirm'));
+}
+
+$tpl->display(sprintf('admin/compta/comptes/%s.tpl', $template));
