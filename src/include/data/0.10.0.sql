0dbd3566bd75c741a3dc1131f9ccab22e736d89d
bohwaz <bohwaz@noemail.net>
2020-01-17 12:11:07 +0000

    Transformation des ID de comptes en codes vers des ID auto-incrémentés, ouvrant la porte à avoir plusieurs comptes avec le même code
    FossilOrigin-Name: be95994256a40c11f9fe8dc2017757aca47ba9ee

--- src/include/data/0.10.0.sql
+++ src/include/data/0.10.0.sql
@@ -1,15 +1,25 @@
 ALTER TABLE compta_journal RENAME TO compta_journal_old;
+ALTER TABLE compta_comptes RENAME TO compta_comptes_old;
 
 .read schema.sql
 
-INSERT INTO compta_journal (id, libelle, remarques, numero_piece, date, moyen_paiement, reference_paiement, id_exercice, id_auteur, id_categorie, id_projet)
+-- Migration comptes de code comme identifiant à ID unique
+INSERT INTO compta_comptes (id, code, parent, libelle, position, plan_comptable, id_exercice)
+	SELECT NULL, id, 0, libelle, position, plan_comptable, NULL FROM compta_comptes_old;
+
+-- Migration de la hiérarchie
+UPDATE compta_comptes AS a SET parent = (SELECT id FROM compta_comptes AS b WHERE code = (SELECT parent FROM compta_comptes_old AS c WHERE id = b.code));
+
+-- Création archives comptes des exercices précédents faite dans upgrade.php !
+
+INSERT INTO compta_mouvements (id, libelle, remarques, numero_piece, date, moyen_paiement, reference_paiement, id_exercice, id_auteur, id_categorie, id_projet)
 	SELECT id, libelle, remarques, numero_piece, date, moyen_paiement, numero_cheque, id_exercice, id_auteur, id_categorie, id_projet FROM compta_journal_old;
 
-INSERT INTO compta_journal_ecritures (id_journal, compte, debit, credit, montant)
-	SELECT id, compte_credit, 0, CAST(montant * 100 AS INT) FROM compta_journal_old;
+INSERT INTO compta_mouvements_lignes (id_mouvement, compte, debit, credit, montant)
+	SELECT id, (SELECT id FROM compta_comptes WHERE code = compte_credit), 0, CAST(montant * 100 AS INT) FROM compta_journal_old;
 
-INSERT INTO compta_journal_ecritures (id_journal, compte, debit, credit, montant)
-	SELECT id, compte_debit, CAST(montant * 100 AS INT), 0 FROM compta_journal_old;
+INSERT INTO compta_mouvements_lignes (id_mouvement, compte, debit, credit, montant)
+	SELECT id, (SELECT id FROM compta_comptes WHERE code = compte_debit), CAST(montant * 100 AS INT), 0 FROM compta_journal_old;
 
 DROP TABLE compta_journal_old;
 
@@ -26,12 +36,8 @@ DROP TABLE compta_journal_old;
 -- CREATE TRIGGER IF NOT EXISTS ON compta_journal_ecritures
 
 -- Transfert des rapprochements
-UPDATE compta_journal_lignes SET rapprochement = 1 WHERE id_journal IN (SELECT id_operation FROM compta_rapprochement);
+UPDATE compta_mouvements_lignes SET rapprochement = 1 WHERE id_mouvement IN (SELECT id_operation FROM compta_rapprochement);
 
 -- Suppression de la table rapprochements
 DROP TABLE compta_rapprochement;
-
--- Ajout moyens de paiement
-INSERT OR IGNORE INTO compta_moyens_paiement (code, nom) VALUES ('AU', 'Autre');
-INSERT OR IGNORE INTO compta_moyens_paiement (code, nom) VALUES ('AC', 'Autres chèques (vacances, cadeau, etc.)');
 
