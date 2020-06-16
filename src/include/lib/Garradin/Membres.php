e191bb250cbb7be4d3deebe254a8d975d536c074
bohwaz <bohwaz@noemail.net>
2020-06-16 11:07:15 +0000

    Fix orthographe, signalé par @Jean-Christophe
    FossilOrigin-Name: 9064396ec5afbd51611bb250058bb2e85baa0b23

--- src/include/lib/Garradin/Membres.php
+++ src/include/lib/Garradin/Membres.php
@@ -150,7 +150,7 @@ class Membres
 
         if (isset($data[$id]) && $db->test('membres', $id . ' = ? COLLATE NOCASE', $data[$id]))
         {
-            throw new UserException('La valeur du champ '.$id.' est déjà utilisée par un autre membre, hors ce champ doit être unique à chaque membre.');
+            throw new UserException('La valeur du champ '.$id.' est déjà utilisée par un autre membre, or ce champ doit être unique à chaque membre.');
         }
 
         if (isset($data['passe']) && trim($data['passe']) != '')
@@ -189,7 +189,7 @@ class Membres
         if (!empty($data[$champ_id])
             && $db->firstColumn('SELECT 1 FROM membres WHERE '.$champ_id.' = ? COLLATE NOCASE AND id != ? LIMIT 1;', $data[$champ_id], (int)$id))
         {
-            throw new UserException('La valeur du champ '.$champ_id.' est déjà utilisée par un autre membre, hors ce champ doit être unique à chaque membre.');
+            throw new UserException('La valeur du champ '.$champ_id.' est déjà utilisée par un autre membre, or ce champ doit être unique à chaque membre.');
         }
 
         if (isset($data['numero']))
