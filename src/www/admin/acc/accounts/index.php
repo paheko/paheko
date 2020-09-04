b9eb26f89154e3ad651032a8fca4fee7dffed6e0
bohwaz <bohwaz@kd2.org>
2020-09-04 20:20:35 +0000

    Liste simplifiée des comptes
    FossilOrigin-Name: 4463ba110d238226743a6e0d0758dfa5dded1f67

--- src/www/admin/acc/accounts/index.php
+++ src/www/admin/acc/accounts/index.php
@@ -3,15 +3,18 @@ namespace Garradin;
 
 use Garradin\Accounting\Charts;
 use Garradin\Accounting\Accounts;
+use Garradin\Entities\Accounting\Account;
 
 require_once __DIR__ . '/../_inc.php';
 
 $session->requireAccess('compta', Membres::DROIT_ADMIN);
 
-$chart = (new Charts)->get(qg('id'));
-$accounts = new Accounts;
+$chart = $year->chart();
+$accounts = $chart->accounts();
 
-$tpl->assign('chart', $chart);
-$tpl->assign('accounts', $accounts->listForChart($chart->id()));
+$tpl->assign('accounts_grouped', $accounts->listCommonGrouped());
 
-$tpl->display('admin/acc/accounts/index.tpl');
+$types = array_filter(Account::TYPES_NAMES, function ($v) { return $v !== ''; });
+$tpl->assign('accounts_types', $types);
+
+$tpl->display('acc/accounts/index.tpl');
