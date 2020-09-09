66d64421b9f315ff26e7b70b9898c035e9d63656
bohwaz <bohwaz@kd2.org>
2020-09-09 19:16:27 +0000

    Make sure a year is selected in accounting pages
    FossilOrigin-Name: 10273757da566310186aea628789060ee6607c62

--- src/www/admin/acc/_inc.php
+++ src/www/admin/acc/_inc.php
@@ -2,6 +2,26 @@
 
 namespace Garradin;
 
+use Garradin\Accounting\Years;
+
 require_once __DIR__ . '/../_inc.php';
 
 $session->requireAccess('compta', Membres::DROIT_ACCES);
+
+if ($year_id = $session->get('acc_year')) {
+	$year = Years::get($year_id);
+}
+else {
+	$year = Years::getCurrentOpenYearIfSingle();
+
+	if ($year) {
+		$year_id = $year->id();
+		$session->set('acc_year', $year_id);
+	}
+	else {
+		Utils::redirect(ADMIN_URL . '/acc/years/?msg=SELECT_YEAR');
+	}
+}
+
+$tpl->assign('year', $year);
+$tpl->assign('chart', $year->chart());
