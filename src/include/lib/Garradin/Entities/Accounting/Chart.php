85376a9b6bfb52f9ee85c7214b687a7ae6464da4
bohwaz <bohwaz@kd2.org>
2020-10-14 17:13:31 +0000

    Implement: add / copy chart
    FossilOrigin-Name: e7d84b2f9e3320f1e7eb89aa1363026989e44f58

--- src/include/lib/Garradin/Entities/Accounting/Chart.php
+++ src/include/lib/Garradin/Entities/Accounting/Chart.php
@@ -3,41 +3,42 @@
 namespace Garradin\Entities\Accounting;
 
 use Garradin\Entity;
+use Garradin\Utils;
 use Garradin\Accounting\Accounts;
 
 class Chart extends Entity
 {
-    const TABLE = 'acc_charts';
+	const TABLE = 'acc_charts';
 
-    protected $id;
-    protected $label;
-    protected $country;
-    protected $code;
-    protected $archived;
+	protected $id;
+	protected $label;
+	protected $country;
+	protected $code;
+	protected $archived = 0;
 
-    protected $_types = [
-        'id'       => 'int',
-        'label'    => 'string',
-        'country'  => 'string',
-        'code'     => '?string',
-        'archived' => 'int',
-    ];
+	protected $_types = [
+		'id'       => 'int',
+		'label'    => 'string',
+		'country'  => 'string',
+		'code'     => '?string',
+		'archived' => 'int',
+	];
 
-    protected $_form_rules = [
-        'label'    => 'required|string|max:200',
-        'country'  => 'required|string|size:2',
-        'code'     => 'string',
-    ];
+	protected $_form_rules = [
+		'label'    => 'required|string|max:200',
+		'country'  => 'required|string|size:2',
+		'code'     => 'string',
+	];
 
-    public function selfCheck(): void
-    {
-        parent::selfCheck();
-        $this->assert(Utils::getCountryName($this->country), 'Le code pays doit être un code ISO valide');
-        $this->assert($this->archived === 0 || $this->archived === 1);
-    }
+	public function selfCheck(): void
+	{
+		parent::selfCheck();
+		$this->assert(Utils::getCountryName($this->country), 'Le code pays doit être un code ISO valide');
+		$this->assert($this->archived === 0 || $this->archived === 1);
+	}
 
-    public function accounts()
-    {
-        return new Accounts($this->id());
-    }
+	public function accounts()
+	{
+		return new Accounts($this->id());
+	}
 }
