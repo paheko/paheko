381da11cf5dd26a1736430eb8aaeda78bd968350
bohwaz <bohwaz@noemail.net>
2020-10-17 07:42:48 +0000

    Implement pie charts
    FossilOrigin-Name: 0fa6b078119902fe5377ddc052c2d9a83ce32917

--- src/www/admin/acc/index.php
+++ src/www/admin/acc/index.php
@@ -13,9 +13,9 @@ $graphs = [
 	ADMIN_URL . 'acc/reports/graph_plot.php?type=assets&year=%s' => 'Évolution banques et caisses',
 	ADMIN_URL . 'acc/reports/graph_plot.php?type=result&year=%s' => 'Évolution dépenses et recettes',
 	ADMIN_URL . 'acc/reports/graph_plot.php?type=debts&year=%s' => 'Évolution dettes et créances',
-	ADMIN_URL . 'acc/reports/pie.php?type=revenue&year=%s' => 'Répartition recettes',
-	ADMIN_URL . 'acc/reports/pie.php?type=expense&year=%s' => 'Répartition dépenses',
-	ADMIN_URL . 'acc/reports/pie.php?type=assets&year=%s' => 'Répartition actif',
+	ADMIN_URL . 'acc/reports/graph_pie.php?type=revenue&year=%s' => 'Répartition recettes',
+	ADMIN_URL . 'acc/reports/graph_pie.php?type=expense&year=%s' => 'Répartition dépenses',
+	ADMIN_URL . 'acc/reports/graph_pie.php?type=assets&year=%s' => 'Répartition actif',
 ];
 
 $tpl->assign('graphs', $graphs);
