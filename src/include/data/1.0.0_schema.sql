0a507ead9f248425cbf814a2c14a4f69704a0590
bohwaz <bohwaz@noemail.net>
2020-10-31 11:21:11 +0000

    Save the date of when a contribution has been paid
    FossilOrigin-Name: 6f98e5f43160f24a5146473b2495bdd114d97bd5

--- src/include/data/1.0.0_schema.sql
+++ src/include/data/1.0.0_schema.sql
@@ -71,7 +71,8 @@ CREATE TABLE IF NOT EXISTS services_users
 
     paid INTEGER NOT NULL DEFAULT 0,
 
-    expiry_date TEXT NULL DEFAULT CURRENT_DATE CHECK (date(expiry_date) IS NOT NULL AND date(expiry_date) = expiry_date)
+    date TEXT NULL DEFAULT CURRENT_DATE CHECK (date(date) IS NOT NULL AND date(date) = date)
+    expiry_date TEXT NULL CHECK (date(expiry_date) IS NULL OR date(expiry_date) = expiry_date)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS su_unique ON services_users (id_user, id_service, expiry_date);
