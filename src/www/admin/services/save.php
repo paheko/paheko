1c209435e97f26cb0f1dc6680abf1ce49878c68a
bohwaz <bohwaz@noemail.net>
2020-11-07 16:01:56 +0000

    Saving a service for a user is working \o/
    FossilOrigin-Name: c4ed8acc1dbb2e561c6d6c819299fcc9e20b0173

--- src/www/admin/services/save.php
+++ src/www/admin/services/save.php
@@ -10,7 +10,17 @@ require_once __DIR__ . '/_inc.php';
 
 $session->requireAccess('membres', Membres::DROIT_ECRITURE);
 
-$user_id = (int) qg('user') ?: null;
+$user_id = qg('user');
+
+if (!$user_id && ($user_id = f('user'))) {
+	$user_id = @key($user_id);
+}
+
+if (!$user_id) {
+	$user_id = f('id_user');
+}
+
+$user_id = (int) $user_id ?: null;
 $user_name = $user_id ? (new Membres)->getNom($user_id) : null;
 
 $grouped_services = Services::listGroupedWithFees($user_id);
@@ -22,13 +32,12 @@ if (!count($grouped_services)) {
 $csrf_key = 'service_save';
 
 $form->runIf('save', function () use ($session) {
-	$su = new Service_User;
-	$su->saveFromForm($session->getUser()->id);
+	$su = Service_User::saveFromForm($session->getUser()->id);
 
 	Utils::redirect(ADMIN_URL . 'services/user.php?id=' . $su->id_user);
 }, $csrf_key);
 
-$selected_user = $user_id ? [$user_id => $user_name] : null;
+$selected_user = $user_name ? [$user_id => $user_name] : null;
 
 $types_details = Transaction::getTypesDetails();
 $account_targets = $types_details[Transaction::TYPE_REVENUE]->accounts[1]->targets_string;
