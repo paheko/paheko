6070373f4ed05b2cb01bb31fd2e3632d8a5974fd
bohwaz <bohwaz@noemail.net>
2020-11-09 21:41:52 +0000

    Add more test case
    FossilOrigin-Name: 7fc961ba1563f14343244548a75656aca833543d

--- tests/unit_tests/02_accounting/money.php
+++ tests/unit_tests/02_accounting/money.php
@@ -11,3 +11,8 @@ Test::strictlyEquals(442, Utils::moneyToInteger('4.42'));
 Test::strictlyEquals(4, Utils::moneyToInteger('0,04'));
 Test::strictlyEquals(30, Utils::moneyToInteger('0,3'));
 Test::strictlyEquals(202034, Utils::moneyToInteger('2020,34'));
+
+Test::strictlyEquals('5,50', Utils::money_format(550));
+Test::strictlyEquals('0,05', Utils::money_format(5));
+Test::strictlyEquals('0,50', Utils::money_format(50));
+Test::strictlyEquals('1 000,50', Utils::money_format(100050));
