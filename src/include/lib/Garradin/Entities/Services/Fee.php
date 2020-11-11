1e0eb13836751c23a85c04793fd9372a4d912ca4
bohwaz <bohwaz@kd2.org>
2020-11-11 15:13:34 +0000

    Fix issue with free/formula fees (thanks @zou!)
    FossilOrigin-Name: 4323146add1fcb063bde4b762227652dfaa8bf1f

--- src/include/lib/Garradin/Entities/Services/Fee.php
+++ src/include/lib/Garradin/Entities/Services/Fee.php
@@ -38,7 +38,7 @@ class Fee extends Entity
 
 	public function filterUserValue(string $type, $value, string $key)
 	{
-		if ($key == 'amount') {
+		if ($key == 'amount' && $value !== null) {
 			$value = Utils::moneyToInteger($value);
 		}
 
@@ -82,7 +82,7 @@ class Fee extends Entity
 		$this->assert(trim($this->label) !== '', 'Le libellé doit être renseigné');
 		$this->assert(strlen($this->label) <= 200, 'Le libellé doit faire moins de 200 caractères');
 		$this->assert(strlen($this->description) <= 2000, 'La description doit faire moins de 2000 caractères');
-		$this->assert(null === $this->amount || $this->amount > 0, 'Le montant est invalide');
+		$this->assert(null === $this->amount || $this->amount > 0, 'Le montant est invalide : ' . $this->amount);
 		$this->assert($this->id_service, 'Aucun service n\'a été indiqué pour ce tarif.');
 		$this->assert((null === $this->id_account && null === $this->id_year)
 			|| (null !== $this->id_account && null !== $this->id_year), 'Le compte doit être indiqué avec l\'exercice');
