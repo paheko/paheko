d10aeca84d1393e6b8aa49edce34705021a21805
bohwaz <bohwaz@kd2.org>
2020-11-18 11:58:59 +0000

    Also delete the files from the deleted year
    FossilOrigin-Name: f5e31ca16745d74d963772b87634a4f7a4f225a9

--- src/include/lib/Garradin/Entities/Accounting/Year.php
+++ src/include/lib/Garradin/Entities/Accounting/Year.php
@@ -3,8 +3,9 @@
 namespace Garradin\Entities\Accounting;
 
 use KD2\DB\EntityManager;
-use Garradin\Entity;
 use Garradin\DB;
+use Garradin\Entity;
+use Garradin\Fichiers;
 use Garradin\UserException;
 use Garradin\Accounting\Accounts;
 
@@ -71,6 +72,9 @@ class Year extends Entity
 	{
 		// Manual delete of transactions, as there is a voluntary safeguard in SQL: no cascade
 		DB::getInstance()->preparedQuery('DELETE FROM acc_transactions WHERE id_year = ?;', $this->id());
+
+		// Clean up files
+		Fichiers::deleteUnlinkedFiles();
 
 		return parent::delete();
 	}
