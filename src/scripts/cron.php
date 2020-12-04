f7e6d742c942d283cb4cc1df9ba9e28638bf16f9
bohwaz <bohwaz@noemail.net>
2020-12-04 18:14:17 +0000

    Move cron to scripts/ directory, add new script to upgrade in CLI
    FossilOrigin-Name: f2ac2bfc2dd652dc1bab4c5494b2dc6025444aea

--- src/scripts/cron.php
+++ src/scripts/cron.php
@@ -4,7 +4,7 @@ namespace Garradin;
 
 use Garradin\Services\Reminders;
 
-require_once __DIR__ . '/include/init.php';
+require_once __DIR__ . '/../include/init.php';
 
 // Exécution des tâches automatiques
 
---
+++ src/scripts/upgrade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Garradin;
+
+const UPGRADE_PROCESS = true;
+
+require_once __DIR__ . '/../include/init.php';
+
+$config = Config::getInstance();
+
+if (version_compare($config->getVersion(), garradin_version(), '<')) {
+	try {
+		if (Upgrade::preCheck()) {
+			Upgrade::upgrade();
+		}
+	}
+	catch (UserException $e) {
+		echo $e->getMessage() . PHP_EOL;
+		exit(1);
+	}
+}
+
+exit(0);
