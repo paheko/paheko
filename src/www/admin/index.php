460b62ee1b206106e3a3cff40bdbc6c67a02352f
bohwaz <bohwaz@kd2.org>
2020-12-12 23:30:57 +0000

    Refactor homepage controller
    FossilOrigin-Name: 4f7a33917eb5210b6cc33011fbbc6394e557d48e

--- src/www/admin/index.php
+++ src/www/admin/index.php
@@ -7,17 +7,12 @@ require_once __DIR__ . '/_inc.php';
 $cats = new Membres\Categories;
 $categorie = $cats->get($user->id_categorie);
 
-$tpl->assign('categorie', $categorie);
-
-$wiki = new Wiki;
-$page = $wiki->getByURI($config->get('accueil_connexion'));
-$tpl->assign('page', $page);
-
-$tpl->assign('custom_css', ['wiki.css']);
+$homepage = Config::getInstance()->get('admin_homepage');
 
 $banner = null;
 Plugin::fireSignal('accueil.banniere', ['user' => $user, 'session' => $session], $banner);
-$tpl->assign('banniere', $banner);
+
+$tpl->assign(compact('categorie', 'homepage', 'banner'));
 
 $tpl->display('admin/index.tpl');
 flush();
