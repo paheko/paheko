1d7b8d823cdfd19c4cc170710b5371438c13a466
bohwaz <bohwaz@kd2.org>
2020-12-12 22:24:02 +0000

    Allow to move transactions past the closing date to another year
    FossilOrigin-Name: addcc82d4afa23901ae7df7596548c17f4a8bb93

--- src/www/admin/acc/years/edit.php
+++ src/www/admin/acc/years/edit.php
@@ -2,6 +2,7 @@
 namespace Garradin;
 
 use Garradin\Accounting\Years;
+use Garradin\Entities\Accounting\Year;
 
 require_once __DIR__ . '/../_inc.php';
 
@@ -17,20 +18,43 @@ if ($year->closed) {
 	throw new UserException('Impossible de modifier un exercice clôturé.');
 }
 
-if (f('edit') && $form->check('acc_years_edit_' . $year->id()))
-{
-	try {
-		$year->importForm();
-		$year->save();
+$csrf_key = 'acc_years_edit_' . $year->id();
 
-		Utils::redirect(ADMIN_URL . 'acc/years/');
-	}
-	catch (UserException $e)
-	{
-		$form->addError($e->getMessage());
-	}
-}
+$form->runIf('edit', function () use ($year) {
+	if (f('split')) {
+		$date = \DateTime::createFromFormat('!d/m/Y', f('end_date'));
 
-$tpl->assign('year', $year);
+		if (!$date) {
+			throw new UserException('Date de séparation invalide');
+		}
+
+		$target = f('split_year');
+
+		if ($target) {
+			$target = Years::get($target);
+		}
+		else {
+			$target = new Year;
+			$new_start = (clone $date)->modify('+1 day');
+	        $target->label = sprintf('Exercice %d', $date->format('Y'));
+	        $target->start_date = $new_start;
+	        $target->end_date = (clone $new_start)->modify('+1 year');
+	        $target->id_chart = $year->id_chart;
+	        $target->save();
+		}
+
+		if (!$target) {
+			throw new UserException('Exercice de séparation invalide');
+		}
+
+		$year->split($date, $target);
+	}
+
+	$year->importForm();
+	$year->save();
+}, $csrf_key, ADMIN_URL . 'acc/years/');
+
+$tpl->assign(compact('year', 'csrf_key'));
+$tpl->assign('split_years', ['' => '-- Créer un nouvel exercice'] + Years::listOpenAssocExcept($year->id()));
 
 $tpl->display('acc/years/edit.tpl');
