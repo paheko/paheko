8f690b2c65850b9f3ab2a0283c75bb00c8757217
bohwaz <bohwaz@kd2.org>
2020-12-16 14:30:23 +0000

    New transaction: Keep rest of the form hidden until a type has been selected
    FossilOrigin-Name: 2c801a6c3f9bba5ab322eb3ce653521e4f4bbe05

--- src/www/admin/acc/transactions/new.php
+++ src/www/admin/acc/transactions/new.php
@@ -17,7 +17,6 @@ $chart = $current_year->chart();
 $accounts = $chart->accounts();
 
 $transaction = new Transaction;
-$transaction->type = -1;
 $lines = [[], []];
 $amount = 0;
 $payoff_for = qg('payoff_for') ?: f('payoff_for');
