aa84852c92301ed0edcff1e665e7ee47b083416f
bohwaz <bohwaz@kd2.org>
2020-12-18 20:18:15 +0000

    Add page delete
    FossilOrigin-Name: 2f6ed7549ece349f4e468e8cdc9d6c50a7c34bb7

--- src/www/admin/web/edit.php
+++ src/www/admin/web/edit.php
@@ -4,6 +4,7 @@ namespace Garradin;
 
 use Garradin\Web;
 use Garradin\Entities\Web\Page;
+use KD2\SimpleDiff;
 
 require_once __DIR__ . '/_inc.php';
 
@@ -11,25 +12,33 @@ $id = (int) qg('id');
 $page = Web::get($id);
 
 if (!$page) {
-    throw new UserException('Page inconnue');
+	throw new UserException('Page inconnue');
 }
 
 $csrf_key = 'edit_' . $page->id();
 $editing_started = f('editing_started') ?: date('Y-m-d H:i:s');
+$diff = null;
 
 if (f('cancel')) {
 	Utils::redirect(ADMIN_URL . 'web/?parent=' . $page->parent_id);
 }
 
-$form->runIf('save', function () use ($page) {
-    $page->importForm();
-    $page->save();
+$form->runIf('save', function () use ($page, $editing_started, &$diff) {
+	$editing_started = new \DateTime($editing_started);
+
+	if ($editing_started < $page->modified) {
+		$diff = SimpleDiff::diff($page->raw(), f('content'));
+		throw new UserException('La page a été modifiée par quelqu\'un d\'autre.');
+	}
+
+	$page->importForm();
+	$page->save();
 }, $csrf_key, Utils::getSelfURI() . '#saved');
 
 $parent = $page->parent_id ? [$page->parent_id => Web::get($page->parent_id)->title] : null;
 $encrypted = f('encrypted') || $page->file()->type == Page::FILE_TYPE_ENCRYPTED;
 
-$tpl->assign(compact('page', 'parent', 'editing_started', 'encrypted', 'csrf_key'));
+$tpl->assign(compact('page', 'parent', 'editing_started', 'encrypted', 'csrf_key', 'diff'));
 
 $tpl->assign('custom_js', ['wiki_editor.js', 'wiki-encryption.js']);
 $tpl->assign('custom_css', ['wiki.css', 'scripts/wiki_editor.css']);
