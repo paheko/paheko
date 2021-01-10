fdcd5fa3fcd3b3e39c25ef0c9c6b7d2b58c267e1
bohwaz <bohwaz@noemail.net>
2021-01-10 02:57:05 +0000

    Move squelettes
    FossilOrigin-Name: 8d56a105c5dfc36644873d169283f04d3a22c2e1

--- src/www/index.php
+++ src/www/index.php
@@ -2,9 +2,7 @@
 
 namespace Garradin;
 
-if (PHP_VERSION_ID < 70200) {
-	die("PHP 7.2 ou supérieur est requis.");
-}
+use Garradin\Web;
 
 require __DIR__ . '/_inc.php';
 
@@ -13,5 +11,4 @@ if (Config::getInstance()->get('desactiver_site'))
 	Utils::redirect(ADMIN_URL);
 }
 
-$squelette = new Squelette;
-$squelette->dispatchURI();
+Web::dispatchURI();
