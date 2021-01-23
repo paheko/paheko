0b8c93fc5190d647ede5418b161563c87c6b1c25
bohwaz <bohwaz@noemail.net>
2021-01-23 00:18:26 +0000

    Rewrite configuration, fix file handling
    FossilOrigin-Name: c4d4b327a98abc64241d6ed28c3643bc0046f11609541f7338c7d99e9b72157b

--- src/www/admin/config/index.php
+++ src/www/admin/config/index.php
@@ -8,34 +8,25 @@ $config = Config::getInstance();
 $form->runIf('save', function () use ($config) {
 	$config->importForm();
 	$config->save();
-}, 'config', Utils::getSelfURI('ok'));
+}, 'config', Utils::getSelfURI(['ok' => '']));
 
-$server_time = time();
+$tpl->assign([
+	'garradin_version' => garradin_version() . ' [' . (garradin_manifest() ?: 'release') . ']',
+	'new_version'      => ENABLE_TECH_DETAILS ? Utils::getLatestVersion() : null,
+	'php_version'      => phpversion(),
+	'has_gpg_support'  => \KD2\Security::canUseEncryption(),
+	'server_time'      => time(),
+	'sqlite_version'   => \SQLite3::version()['versionString'],
+	'countries'        => Utils::getCountryList(),
+	'membres_cats'     => (new Membres\Categories)->listSimple(),
+	'champs'           => $config->get('champs_membres')->listAssocNames(),
+	'color1'           => ADMIN_COLOR1,
+	'color2'           => ADMIN_COLOR2,
+]);
 
-$tpl->assign('garradin_version', garradin_version() . ' [' . (garradin_manifest() ?: 'release') . ']');
+$image_fond = $config->get('image_fond') ? $config->get('image_fond')->url() : null;
 
-$tpl->assign('new_version', ENABLE_TECH_DETAILS ? Utils::getLatestVersion() : null);
-$tpl->assign('php_version', phpversion());
-$tpl->assign('has_gpg_support', \KD2\Security::canUseEncryption());
-$tpl->assign('server_time', $server_time);
-
-$v = \SQLite3::version();
-$tpl->assign('sqlite_version', $v['versionString']);
-
-$tpl->assign('countries', Utils::getCountryList());
-
-$cats = new Membres\Categories;
-$tpl->assign('membres_cats', $cats->listSimple());
-
-$tpl->assign('champs', $config->get('champs_membres')->getList());
-
-$tpl->assign('couleur1', $config->get('couleur1') ?: ADMIN_COLOR1);
-$tpl->assign('couleur2', $config->get('couleur2') ?: ADMIN_COLOR2);
-
-$image_fond = $config->get('image_fond') ? $config->get('image_fond')->url() : ADMIN_BACKGROUND_IMAGE;
-
-$tpl->assign('background_image_source', $image_fond);
-$tpl->assign('background_image_current', f('image_fond') ?: $image_fond);
+$tpl->assign('background_image_current', $image_fond);
 $tpl->assign('background_image_default', ADMIN_BACKGROUND_IMAGE);
 
 $tpl->assign('custom_js', ['color_helper.js']);
