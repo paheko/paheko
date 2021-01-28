77dca341fca9dd225f0d05427ff74392fe4c29bd
bohwaz <bohwaz@kd2.org>
2021-01-28 15:08:29 +0000

    Store version in user_version pragma now
    FossilOrigin-Name: ddc306861e933a9ddd6a5cc7a2af44d43f25abc620b8641f2f9dff86a70ead38

--- src/include/lib/Garradin/DB.php
+++ src/include/lib/Garradin/DB.php
@@ -14,6 +14,8 @@ class DB extends SQLite3
 
     static protected $_instance = null;
 
+    protected $_version = -1;
+
     static public function getInstance($create = false, $readonly = false)
     {
         if (null === self::$_instance) {
@@ -48,6 +50,66 @@ class DB extends SQLite3
         $this->exec(sprintf('PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL; PRAGMA journal_size_limit = %d;', 32 * 1024 * 1024));
 
         $this->db->createFunction('transliterate_to_ascii', ['Garradin\Utils', 'transliterateToAscii']);
+    }
+
+    public function version(): ?string
+    {
+        if (-1 === $this->_version) {
+            $this->connect();
+            $v = (int) $this->db->querySingle('PRAGMA user_version;');
+            $v = self::parseVersion($v);
+
+            if (null === $v) {
+                // For legacy version before 1.1.0
+                $v = $this->db->querySingle('SELECT valeur FROM config WHERE cle = \'version\';');
+            }
+
+            $this->_version = $v ?: null;
+        }
+
+        return $this->_version;
+    }
+
+    static public function parseVersion(int $v): ?string
+    {
+        if ($v > 0) {
+            $major = intval($v / 1000000);
+            $v -= $major * 1000000;
+            $minor = intval($v / 10000);
+            $v -= $minor * 10000;
+            $release = intval($v / 100);
+            $v -= $release * 100;
+            $type = $v;
+
+            if ($type == 0) {
+                $type = '';
+            }
+            elseif ($type > 50) {
+                $type = '-rc' . ($type - 50);
+            }
+            else {
+                $type = '-beta' . $type;
+            }
+
+            $v = sprintf('%d.%d.%d%s', $major, $minor, $release, $type);
+        }
+
+        return $v ?: null;
+    }
+
+    /**
+     * Save version to database
+     * Only rc and beta strings are allowed, others will throw an error
+     * @param string $version Version string, eg. 1.2.3-rc2
+     */
+    public function setVersion(string $version): void
+    {
+        if (!preg_match('/^\d+\.\d+\.\d+(?:-(?:beta|rc)(\d+))?$/', $version, $match)) {
+            throw new \InvalidArgumentException('Invalid version number: ' . $version);
+        }
+
+        $version = ($match[1] * 100 * 100 * 100) + ($match[2] * 100 * 100) + ($match[3] * 100) + ($match[4]);
+        $this->db->exec(sprintf('PRAGMA user_version = %d;', $version));
     }
 
     public function close(): void
