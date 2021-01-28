33d6872882b81159ea7aa40c97778256ee066369
bohwaz <bohwaz@kd2.org>
2021-01-28 17:59:10 +0000

    Fix indentation
    FossilOrigin-Name: 22c6bb58f6502b8e84ee39b3f58019e607d6ffd47f1fc01a89a081589e398ba0

--- src/www/admin/config/categories/supprimer.php
+++ src/www/admin/config/categories/supprimer.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../_inc.php';
 $cat = Categories::get((int) qg('id'));
 
 if (!$cat) {
-    throw new UserException("Cette catégorie n'existe pas.");
+	throw new UserException("Cette catégorie n'existe pas.");
 }
 
 $user = $session->getUser();
@@ -16,11 +16,11 @@ $user = $session->getUser();
 $csrf_key = 'cat_delete_' . $cat->id();
 
 if ($cat->id() == $user->category_id) {
-    throw new UserException("Vous ne pouvez pas supprimer votre catégorie.");
+	throw new UserException("Vous ne pouvez pas supprimer votre catégorie.");
 }
 
 $form->runIf('delete', function () use($cat) {
-    $cat->delete();
+	$cat->delete();
 }, $csrf_key, '!config/categories/');
 
 $tpl->assign(compact('cat', 'csrf_key'));
