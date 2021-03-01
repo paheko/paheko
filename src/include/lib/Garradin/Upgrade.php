64c8ea2405384b39b670076f82dc397afcbb5dba
bohwaz <bohwaz@kd2.org>
2021-03-01 16:08:26 +0000

    Version 1.0.6: make sure that debts/credits payment transactions are treated as advanced
    FossilOrigin-Name: 322e184f4b34de0b651f59c6c5c344d4b62d317cc96252862a568ddcb5a0cd56

--- src/include/lib/Garradin/Upgrade.php
+++ src/include/lib/Garradin/Upgrade.php
@@ -142,6 +142,14 @@ class Upgrade
 				$db->commit();
 			}
 
+			if (version_compare($v, '1.0.6', '<'))
+			{
+				// Missing trigger
+				$db->begin();
+				$db->import(ROOT . '/include/data/1.0.6_migration.sql');
+				$db->commit();
+			}
+
 			// Vérification de la cohérence des clés étrangères
 			$db->foreignKeyCheck();
 
