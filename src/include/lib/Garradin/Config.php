281d3ff2e2d9a47e0d963ef710affef31f94fbf9
bohwaz <bohwaz@kd2.org>
2021-04-08 14:28:56 +0000

    Fix change of id field
    FossilOrigin-Name: 3bbe7a49e59e0b671a39da7b6c801590172581454e36ab6ebe53dca2c11f7349

--- src/include/lib/Garradin/Config.php
+++ src/include/lib/Garradin/Config.php
@@ -161,9 +161,9 @@ class Config extends Entity
 			$db->exec('UPDATE membres SET '.$this->get('champ_identifiant').' = NULL
 				WHERE '.$this->get('champ_identifiant').' = "";');
 
-			// Création de l'index unique
-			$db->exec('DROP INDEX IF EXISTS membres_identifiant;');
-			$db->exec('CREATE UNIQUE INDEX membres_identifiant ON membres ('.$this->get('champ_identifiant').');');
+			// Création de l'index unique / FIXME move to Champs
+			$db->exec('DROP INDEX IF EXISTS users_id_field;');
+			$db->exec('CREATE UNIQUE INDEX users_id_field ON membres ('.$this->get('champ_identifiant').');');
 		}
 
 		$db->commit();
@@ -246,7 +246,7 @@ class Config extends Entity
 		$this->assert(!empty($champs->get($this->champ_identifiant)), sprintf('Le champ spécifié pour identifiant, "%s" n\'existe pas', $this->champ_identifiant));
 
 		$db = DB::getInstance();
-		$sql = sprintf('SELECT (COUNT(DISTINCT %s) = COUNT(*)) FROM membres WHERE %1$s IS NOT NULL AND %1$s != \'\';', $this->champ_identifiant);
+		$sql = sprintf('SELECT (COUNT(DISTINCT LOWER(%s)) = COUNT(*)) FROM membres WHERE %1$s IS NOT NULL AND %1$s != \'\';', $this->champ_identifiant);
 		$is_unique = $db->firstColumn($sql);
 
 		$this->assert($is_unique, sprintf('Le champ "%s" comporte des doublons et ne peut donc pas servir comme identifiant unique de connexion.', $this->champ_identifiant));
