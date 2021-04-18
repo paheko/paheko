7a3070d85a186dfd8ed22356dd5f17b9da79a61f
bohwaz <bohwaz@noemail.net>
2021-04-18 10:35:28 +0000

    Improve admin_homepage default text
    FossilOrigin-Name: 8b545bdf395bb62c377730ad9a17d8e9fb5c7b08110262ce747413576a904e6a

--- src/include/lib/Garradin/Install.php
+++ src/include/lib/Garradin/Install.php
@@ -147,7 +147,7 @@ class Install
 			'pays'        => 'FR',
 		]);
 
-		$welcome_text = $welcome_text ?? sprintf("Bienvenue dans l'administration de %s !\n\nUtilisez le menu à gauche pour accéder aux différentes sections.", $name);
+		$welcome_text = $welcome_text ?? sprintf("Bienvenue dans l'administration de %s !\n\nUtilisez le menu à gauche pour accéder aux différentes sections.\n\nCe message peut être modifié dans la 'Configuration'.", $name);
 
 		$file = File::createAndStore(File::CONTEXT_CONFIG, 'admin_homepage.skriv', null, $welcome_text);
 		$config->set('admin_homepage', $file->path);
