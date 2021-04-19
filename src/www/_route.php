9ddf827326d3fd4ff5421582925782daf0017e75
bohwaz <bohwaz@noemail.net>
2021-04-19 10:21:50 +0000

    Send correct HTTP code for errors
    FossilOrigin-Name: 5289bbe021beafb3eff2c0ddb976896dbe86978749c76492c5f187097767fc55

--- src/www/_route.php
+++ src/www/_route.php
@@ -3,14 +3,18 @@
 namespace Garradin;
 
 if (empty($_SERVER['REQUEST_URI'])) {
+	http_response_code(500);
 	die('Appel non supporté');
 }
 
 $uri = $_SERVER['REQUEST_URI'];
 
 if ('_route.php' === basename($uri)) {
+	http_response_code(403);
 	die('Appel interdit');
 }
+
+http_response_code(200);
 
 if ('favicon.ico' === basename($uri)) {
 	die('');
@@ -24,6 +28,7 @@ if (($pos = strpos($uri, '?')) !== false)
 if (file_exists(__DIR__ . $uri))
 {
 	if (PHP_SAPI != 'cli-server') {
+		http_response_code(500);
 		die('Erreur de configuration du serveur web: cette URL ne devrait pas être traitée par Garradin');
 	}
 
