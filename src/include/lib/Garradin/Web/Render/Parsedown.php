f7c7c24ec13e9bbfac1970177fc50be07fb192f2
bohwaz <bohwaz@noemail.net>
2021-05-20 09:11:43 +0000

    Implement our own lighter TOC function instead of using ParsedownToc
    FossilOrigin-Name: 153b3b51b57d67bbcdacb28b9d0bd57315851ce62b995031a9383aa6c5c68911

--- src/include/lib/Garradin/Web/Render/Parsedown.php
+++ src/include/lib/Garradin/Web/Render/Parsedown.php
@@ -3,18 +3,27 @@
 namespace Garradin\Web\Render;
 
 //use Parsedown;
-//use ParsedownExtra;
-use ParsedownToc;
+use ParsedownExtra;
 
 use Garradin\Entities\Files\File;
 
-class Parsedown extends ParsedownToc
+use Garradin\Utils;
+
+/**
+ * Custom Parsedown extension to enable the use of Skriv extensions inside Markdown markup
+ *
+ * @see https://github.com/erusev/parsedown/wiki/Tutorial:-Create-Extensions
+ */
+class Parsedown extends ParsedownExtra
 {
 	protected $skriv;
+	protected $rawTextTOC;
 
 	function __construct(?File $file)
 	{
 		$this->BlockTypes['<'][] = 'SkrivExtension';
+		$this->BlockTypes['['][]= 'TOC';
+
 		parent::__construct();
 		$this->skriv = new Skriv;
 
@@ -23,7 +32,7 @@ class Parsedown extends ParsedownToc
 		}
 	}
 
-	function blockSkrivExtension(array $line, array $block): ?array
+	protected function blockSkrivExtension(array $line): ?array
 	{
 		$line = $line['text'];
 
@@ -31,12 +40,12 @@ class Parsedown extends ParsedownToc
 			$text = $this->skriv->callExtension($match);
 
 			return [
-				'char' => $line[0],
-				'element' => array(
-					'name' => 'div',
-					'rawHtml' => $text,
+				'char'    => $line[0],
+				'element' => [
+					'name'                   => 'div',
+					'rawHtml'                => $text,
 					'allowRawHtmlInSafeMode' => true,
-				),
+				],
 				'complete' => true,
 			];
 		}
@@ -44,9 +53,83 @@ class Parsedown extends ParsedownToc
 		return null;
 	}
 
-	protected function blockSkrivExtensionComplete(array $block): ?array
+	protected function blockHeader($line)
 	{
-		var_dump($block);
+		$block = parent::blockHeader($line);
+
+		if (is_array($block)) {
+			if (!isset($block['element']['attributes']['id'])) {
+				$block['element']['attributes']['id'] = Utils::transformTitleToURI($block['element']['text']);
+			}
+
+			$level = substr($block['element']['name'], 1); // h1, h2... -> 1, 2...
+			$id = $block['element']['attributes']['id'];
+			$label = $block['element']['text'];
+
+			$this->toc[] = compact('level', 'id', 'label');
+		}
+
 		return $block;
+	}
+
+	protected function blockTOC(array $line): ?array
+	{
+		if (false === strpos($line['text'], '[toc]')) {
+			return null;
+		}
+
+		return [
+			'char'     => $line['text'][0],
+			'complete' => true,
+			'element'  => [
+				'name'                   => 'div',
+				'rawHtml'                => '<toc></toc>',
+				'allowRawHtmlInSafeMode' => true,
+			],
+		];
+	}
+
+	public function buildTOC(): string
+	{
+		if (!count($this->toc)) {
+			return '';
+		}
+
+		$out = '<div class="toc">';
+
+		$level = 0;
+
+		foreach ($this->toc as $h) {
+			if ($h['level'] > $level) {
+				$out .= str_repeat('<ol>', $h['level'] - $level);
+				$level = $h['level'];
+			}
+			elseif ($h['level'] < $level) {
+				$out .= str_repeat('</ol>', $level - $h['level']);
+				$level = $h['level'];
+			}
+
+			$out .= sprintf('<li><a href="#%s">%s</a></li>', $h['id'], $h['label']);
+		}
+
+		if ($level > 0) {
+			$out .= str_repeat('</ol>', $level);
+		}
+
+		$out .= '</div>';
+
+		return $out;
+	}
+
+	public function text($text)
+	{
+		$out = parent::text($text);
+
+		if (false !== strpos($out, '<toc></toc>')) {
+			$toc = $this->buildTOC();
+			$out = str_replace('<toc></toc>', $toc, $out);
+		}
+
+		return $out;
 	}
 }
