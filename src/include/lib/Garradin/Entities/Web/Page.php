4e89320fd7926d71159ef804e600bf743cde352e
bohwaz <bohwaz@noemail.net>
2021-11-29 15:40:06 +0000

    Fix: also update file_path when moving a category
    FossilOrigin-Name: ed4415500283b2ac0515da9142deaa2924887fcda90598eee031a994d3ad2829

--- src/include/lib/Garradin/Entities/Web/Page.php
+++ src/include/lib/Garradin/Entities/Web/Page.php
@@ -222,7 +222,8 @@ class Page extends Entity
 			$sql = sprintf('UPDATE web_pages
 				SET
 					path = %1$s || substr(path, %2$d),
-					parent = %1$s || substr(parent, %2$d)
+					parent = %1$s || substr(parent, %2$d),
+					file_path = \'web/\' || %1$s || substr(file_path, %2$d + 4)
 				WHERE path LIKE %3$s;',
 				$db->quote($this->path), strlen($change_parent) + 1, $db->quote($change_parent . '/%'));
 			$db->exec($sql);
