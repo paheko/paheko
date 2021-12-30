b8cdeb9e0187990432f2e2681cf7cc3da457eac9
bohwaz <bohwaz@kd2.org>
2021-12-30 12:21:18 +0000

    Service subscription: don't select a service if none has been selected
    FossilOrigin-Name: 7a0c010a937bd1f80c51cad5257aaab20d5fda2a5f1a1ede44bed8b8fbdff4ec

--- src/www/admin/services/user/add.php
+++ src/www/admin/services/user/add.php
@@ -17,7 +17,7 @@ if (!$count_all) {
 	Utils::redirect(ADMIN_URL . 'services/?CREATE');
 }
 
-$services = Services::listAssoc();
+$services = array_merge([0 => '-- Sélectionner une activité'], Services::listAssoc());
 
 $tpl->assign(compact('services'));
 
