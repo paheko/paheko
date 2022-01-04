195432a022cc96ac8634b05f8427e165d4934670
bohwaz <bohwaz@kd2.org>
2022-01-04 20:55:33 +0000

    Improve date picker widget: allow to select month and year from calendar, auto-complete the text input, and make it more accessible and easier to close
    FossilOrigin-Name: 14d9a17071ee1793ce0e15c32bed03acfd5cbf6bbe18ba04d091702780dcad98

--- src/www/admin/static/scripts/global.js
+++ src/www/admin/static/scripts/global.js
@@ -301,6 +301,7 @@
 		var btn = document.createElement('button');
 		var cal = null;
 		btn.className = 'icn-btn';
+		btn.title = 'Cliquer pour ouvrir le calendrier. Utiliser les flèches du clavier pour sélectionner une date, et page précédente suivante pour changer de mois.';
 		btn.setAttribute('data-icon', '📅');
 		btn.type = 'button';
 		btn.onclick = () => {
@@ -313,6 +314,23 @@
 		};
 		span.appendChild(btn);
 		input.parentNode.insertBefore(span, input.nextSibling);
+
+		const getCaretPosition = e => e && e.selectionStart || -1;
+
+		const inputKeyEvent = (e) => {
+			if (input.value.match(/^\d$|^\d\d?\/\d$/) && e.key.match(/^[0-9]$/)) {
+				input.value += e.key + '/';
+				e.preventDefault();
+				return false;
+			}
+
+			if (e.key == '/' && input.value.slice(-1) == '/') {
+				e.preventDefault();
+				return false;
+			}
+
+		};
+		input.addEventListener('keydown', inputKeyEvent, true);
 	};
 
 	g.current_list_input = null;
