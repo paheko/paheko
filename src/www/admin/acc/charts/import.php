e8d7ea232fc21560f14128cee1b539b183b82002
bohwaz <bohwaz@kd2.org>
2022-02-17 16:08:04 +0000

    Use a transaction to create the chart before importing accounts so that if something fails the transaction will be reverted
    FossilOrigin-Name: be0e92b7979bd1053a2a2f875affc85e90063c79b3d7bb90dab0e17c10db5289

--- src/www/admin/acc/charts/import.php
+++ src/www/admin/acc/charts/import.php
@@ -10,10 +10,15 @@ require_once __DIR__ . '/../_inc.php';
 $session->requireAccess($session::SECTION_ACCOUNTING, $session::ACCESS_ADMIN);
 
 $form->runIf('import', function () {
+	$db = DB::getInstance();
+	$db->begin();
+
 	$chart = new Chart;
 	$chart->importForm();
 	$chart->save();
 	$chart->accounts()->importUpload($_FILES['file']); // This will save everything
+
+	$db->commit();
 }, 'acc_charts_import', '!acc/charts/');
 
 $tpl->assign('columns', implode(', ', Accounts::EXPECTED_CSV_COLUMNS));
