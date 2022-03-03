50d970ce563089f48354aa4813f19bc648ef246c
bohwaz <bohwaz@kd2.org>
2022-03-03 17:59:38 +0000

    Refactoring: remove getClosingSumsWithAccounts, use getAccountsBalance, fix graphs
    FossilOrigin-Name: a1ef61e8d71e32281cf35566f261c617d425dfd2febb967aee7975d71fd20be2

--- src/include/lib/Garradin/Accounting/Graph.php
+++ src/include/lib/Garradin/Accounting/Graph.php
@@ -35,10 +35,10 @@ class Graph
 
 	const PLOT_TYPES = [
 		'assets' => [
-			'Total' => ['type' => [Account::TYPE_BANK, Account::TYPE_CASH, Account::TYPE_OUTSTANDING]],
+			'Total' => ['type' => [Account::TYPE_BANK, Account::TYPE_CASH, Account::TYPE_OUTSTANDING], 'exclude_position' => [Account::LIABILITY]],
 			'Banques' => ['type' => Account::TYPE_BANK],
 			'Caisses' => ['type' => Account::TYPE_CASH],
-			'En attente' => ['type' => Account::TYPE_OUTSTANDING],
+			'En attente' => ['type' => Account::TYPE_OUTSTANDING, 'exclude_position' => [Account::LIABILITY]],
 		],
 		'result' => [
 			'Recettes' => ['position' => Account::REVENUE],
@@ -130,7 +130,7 @@ class Graph
 		$pie = new Pie(700, 300);
 
 		$pie_criterias = self::PIE_TYPES[$type];
-		$data = Reports::getClosingSumsWithAccounts(array_merge($criterias, $pie_criterias), 'ABS(sum) DESC');
+		$data = Reports::getAccountsBalances(array_merge($criterias, $pie_criterias), 'balance DESC');
 
 		$others = 0;
 		$colors = self::getColors();
@@ -140,23 +140,22 @@ class Graph
 		$i = 0;
 
 		foreach ($data as $row) {
-			$row->sum = abs($row->sum);
-			$total += $row->sum;
+			$total += $row->balance;
 		}
 
 		foreach ($data as $row)
 		{
 			if ($i++ >= $max || $count > $total*0.95)
 			{
-				$others += $row->sum;
+				$others += $row->balance;
 			}
 			else
 			{
 				$label = strlen($row->label) > 40 ? substr($row->label, 0, 38) . '…' : $row->label;
-				$pie->add(new Pie_Data(abs($row->sum) / 100, $label, $colors[$i-1]));
+				$pie->add(new Pie_Data(abs($row->balance) / 100, $label, $colors[$i-1]));
 			}
 
-			$count += $row->sum;
+			$count += $row->balance;
 		}
 
 		if ($others != 0)
@@ -195,13 +194,6 @@ class Graph
 				continue;
 			}
 
-			// Invert sums for banks, cash, etc.
-			if ('assets' === $type || 'debts' === $type || ('result' === $type && $line_criterias['position'] == Account::EXPENSE)) {
-				array_walk($years, function (&$v) { $v->sum = $v->sum * -1; });
-			}
-
-			array_walk($years, function (&$v) { $v->sum = (int)$v->sum/100; });
-
 			foreach ($years as $year) {
 				$start = Utils::date_fr($year->start_date, 'Y');
 				$end = Utils::date_fr($year->end_date, 'Y');
@@ -213,7 +205,7 @@ class Graph
 					$data[$year_id] = new Bar_Data_Set($year_label);
 				}
 
-				$data[$year_id]->add($year->sum, $label, $color);
+				$data[$year_id]->add((int) $year->balance / 100, $label, $color);
 			}
 		}
 
