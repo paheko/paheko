57410b87155dff0053f01efe8b9ea7ddc316356f
bohwaz <bohwaz@kd2.org>
2022-04-04 23:45:47 +0000

    Rename file
    FossilOrigin-Name: 36d4c29e0dcd97578aa64e102d7b703cf98dfcdcb2f5abd4e6d94d0ae6c7bc08

--- tools/factory/factory_cron.sh
+++ tools/factory/factory_cron.sh
@@ -2,13 +2,13 @@
 
 # Répertoire où sont stockées les données des utilisateurs
 # veiller à ce que ce soit le même que dans config.local.php
-USINE_USER_DIRECTORY="/.../garradin/users"
+FACTORY_USER_DIRECTORY="/.../garradin/users"
 
 # Chemin vers le script cron.php de Garradin
 GARRADIN_CRON_SCRIPT="/.../scripts/cron.php"
 
-for user in $(cd ${USINE_USER_DIRECTORY} && ls -1d */)
+for user in $(cd ${FACTORY_USER_DIRECTORY} && ls -1d */)
 do
-	GARRADIN_USINE_USER=$(basename "$user")
+	GARRADIN_FACTORY_USER=$(basename "$user")
 	php $GARRADIN_CRON_SCRIPT
 done
