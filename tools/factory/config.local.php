326342383a73c9581d23a6624f34a7659855306d
bohwaz <bohwaz@kd2.org>
2022-04-04 23:45:47 +0000

    Rename file
    FossilOrigin-Name: 36d4c29e0dcd97578aa64e102d7b703cf98dfcdcb2f5abd4e6d94d0ae6c7bc08

--- tools/factory/config.local.php
+++ tools/factory/config.local.php
@@ -19,7 +19,7 @@ namespace Garradin;
 // Nom de domaine parent des associations hébergées
 // Exemple : si vos associations sont hébergées en clubdetennis.garradin.eu,
 // indiquer ici 'garradin.eu'
-const USINE_DOMAIN = 'monsite.tld';
+const FACTORY_DOMAIN = 'monsite.tld';
 
 // Répertoire où seront stockées les données des utilisateurs
 // Dans ce répertoire, un sous-répertoire sera créé pour chaque compte
@@ -28,16 +28,18 @@ const USINE_DOMAIN = 'monsite.tld';
 //
 // Pour chaque utilisateur il faudra créer le sous-répertoire en premier lieu
 // (eg. mkdir .../users/clubdetennis)
-const USINE_USER_DIRECTORY = __DIR__ . '/users';
+const FACTORY_USER_DIRECTORY = __DIR__ . '/users';
 
-// Envoyer les erreurs PHP par mail à cette adresse (conseillé)
+// Envoyer les erreurs PHP par mail à l'adresse de l'administrateur système
+// (mettre à null pour ne pas recevoir d'erreurs)
 const MAIL_ERRORS = 'administrateur@monsite.tld';
 
+// IMPORTANT !
 // Modifier pour indiquer une valeur aléatoire de plus de 30 caractères
 const SECRET_KEY = 'Indiquer ici une valeur aléatoire !';
 
 ////////////////////////////////////////////////////////////////
-// Réglages (plus que) conseillés
+// Réglages conseillés, normalement il n'y a rien à modifier ici
 
 // Indiquer que l'on va utiliser cron pour lancer les tâches à exécuter (envoi de rappels de cotisation)
 const USE_CRON = true;
@@ -62,13 +64,13 @@ $login = null;
 
 // Un sous-domaine ne peut pas faire plus de 63 caractères
 $login_regexp = '([a-z0-9_-]{1,63})';
-$domain_regexp = sprintf('/^%s\.%s$/', $login_regexp, preg_quote(USINE_DOMAIN, '/'));
+$domain_regexp = sprintf('/^%s\.%s$/', $login_regexp, preg_quote(FACTORY_DOMAIN, '/'));
 
 if (isset($_SERVER['SERVER_NAME']) && preg_match($regexp, $_SERVER['SERVER_NAME'], $match)) {
 	$login = $match[1];
 }
-elseif (PHP_SAPI == 'cli' && !empty($_SERVER['GARRADIN_USINE_USER']) && preg_match('/^' . $login_regexp . '$/', $_SERVER['GARRADIN_USINE_USER'])) {
-	$login = $_SERVER['GARRADIN_USINE_USER'];
+elseif (PHP_SAPI == 'cli' && !empty($_SERVER['GARRADIN_FACTORY_USER']) && preg_match('/^' . $login_regexp . '$/', $_SERVER['GARRADIN_FACTORY_USER'])) {
+	$login = $_SERVER['GARRADIN_FACTORY_USER'];
 }
 else {
 	// Login invalide ou non fourni
@@ -76,7 +78,7 @@ else {
 	die('<h1>Page non trouvée</h1>');
 }
 
-$user_data_dir = rtrim(USINE_USER_DIRECTORY, '/') . '/' . $login;
+$user_data_dir = rtrim(FACTORY_USER_DIRECTORY, '/') . '/' . $login;
 
 if (!is_dir($user_data_dir)) {
 	http_response_code(404);
