ad717d6d1442d67b5263f333e8984db9af6655cc
bohwaz <bohwaz@kd2.org>
2022-06-07 14:40:48 +0000

    Add list of debts and credits for a user
    FossilOrigin-Name: 328a714b2c5c34ce6b7e5c7d3793cfd37623520c1def68ff43f3333461502f7f

--- src/www/admin/me/services.php
+++ src/www/admin/me/services.php
@@ -2,6 +2,8 @@
 namespace Garradin;
 
 use Garradin\Services\Services_User;
+use Garradin\Accounting\Reports;
+use Garradin\Entities\Accounting\Account;
 
 require_once __DIR__ . '/../_inc.php';
 
@@ -13,5 +15,6 @@ $list->loadFromQueryString();
 $tpl->assign(compact('list'));
 
 $tpl->assign('services', Services_User::listDistinctForUser($user->id));
+$tpl->assign('accounts', Reports::getAccountsBalances(['user' => $user->id, 'type' => Account::TYPE_THIRD_PARTY]));
 
 $tpl->display('me/services.tpl');
