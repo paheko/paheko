7ed3451f81d45a53288e825823c45360f1c818e4
bohwaz <bohwaz@kd2.org>
2022-06-22 12:19:43 +0000

    Fix simple view for all, fix [e8c01de2d7e5b8fca77684001df4035f9fbdeb69]
    FossilOrigin-Name: 0b18f0c0f489ca291803e188815c3f818aa8a9e6ec2b8e7a054d397e6cd57d8d

--- src/www/admin/acc/accounts/simple.php
+++ src/www/admin/acc/accounts/simple.php
@@ -13,7 +13,7 @@ if (!CURRENT_YEAR_ID) {
 $year = $current_year;
 
 $types = [
-	null => 'Toutes',
+	-1 => 'Toutes',
 	Transaction::TYPE_REVENUE => 'Recettes',
 	Transaction::TYPE_EXPENSE => 'Dépenses',
 	Transaction::TYPE_TRANSFER => 'Virements',
@@ -22,13 +22,13 @@ $types = [
 	Transaction::TYPE_ADVANCED => 'Saisies avancées',
 ];
 
-$type = qg('type') ?: null;
+$type = qg('type');
 
 if (!array_key_exists($type, $types)) {
 	$type = key($types);
 }
 
-$list = Transactions::listByType(CURRENT_YEAR_ID, $type);
+$list = Transactions::listByType(CURRENT_YEAR_ID, $type == -1 ? null : $type);
 $list->setTitle(sprintf('Suivi - %s', $types[$type]));
 $list->loadFromQueryString();
 
