b8fe6f5e41e167d4a38c9ef77c8bade7999415db
bohwaz <bohwaz@kd2.org>
2022-07-12 19:15:48 +0000

    Make sure to escape links
    FossilOrigin-Name: 1c7f30a616f764fe8dba0ff1874f80d516819784689cb5c0c7971cd7ad34f3fa

--- src/include/lib/Garradin/Web/Render/Skriv.php
+++ src/include/lib/Garradin/Web/Render/Skriv.php
@@ -46,7 +46,7 @@ class Skriv extends AbstractRender
 		$str = CommonModifiers::typo($str);
 
 		$str = preg_replace_callback(';<a href="((?!https?://|\w+:).+)">;i', function ($matches) {
-			return sprintf('<a href="%s" target="_parent">', $this->resolveLink($matches[1]));
+			return sprintf('<a href="%s" target="_parent">', htmlspecialchars($this->resolveLink($matches[1])));
 		}, $str);
 
 		return sprintf('<div class="web-content">%s</div>', $str);
