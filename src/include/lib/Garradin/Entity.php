c28a55b47e33fe8b748e0cc579a1abc5d9978071
bohwaz <bohwaz@kd2.org>
2022-07-18 22:33:55 +0000

    Try to handle Y/m/d dates
    FossilOrigin-Name: e9cbaea9393b2f6923fc50dbacaf51b19ffd172efdd87350c20cb9f25750f1e5

--- src/include/lib/Garradin/Entity.php
+++ src/include/lib/Garradin/Entity.php
@@ -44,6 +44,9 @@ class Entity extends AbstractEntity
 			elseif (preg_match('!^\d{2}/\d{2}/\d{4}$!', $value)) {
 				return \DateTime::createFromFormat('d/m/Y', $value);
 			}
+			elseif (preg_match('!^\d{4}/\d{2}/\d{2}$!', $value)) {
+				return \DateTime::createFromFormat('Y/m/d', $value);
+			}
 			elseif (null !== $value) {
 				throw new ValidationException('Format de date invalide (merci d\'utiliser le format JJ/MM/AAAA) : ' . $value);
 			}
