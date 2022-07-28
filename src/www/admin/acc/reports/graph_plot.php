c64881e4fcf13e9967ab41201e75714a7a57d070
bohwaz <bohwaz@kd2.org>
2022-07-28 14:41:05 +0000

    Force graph update
    FossilOrigin-Name: 61b70ea0de882a9adf0eb470d7a03718507306f83e7ab1febe8d9e6122cff15c

--- src/www/admin/acc/reports/graph_plot.php
+++ src/www/admin/acc/reports/graph_plot.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/_inc.php';
 header('Content-Type: image/svg+xml');
 
 $expiry = time() - 30;
-$hash = sha1('plot_' . json_encode($criterias));
+$hash = sha1('graph_plot_' . json_encode($criterias));
 
 if (!Utils::HTTPCache($hash, $expiry)) {
 	echo Graph::plot(qg('type'), $criterias);
--- src/www/admin/acc/reports/graph_plot_all.php
+++ src/www/admin/acc/reports/graph_plot_all.php
@@ -10,7 +10,7 @@ qv(['type' => 'string|required']);
 header('Content-Type: image/svg+xml');
 
 $expiry = time() - 30;
-$hash = sha1('plot_all');
+$hash = sha1('graph_plot_all');
 
 if (!Utils::HTTPCache($hash, $expiry)) {
 	echo Graph::bar(qg('type'), []);
