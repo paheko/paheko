44105580a3d9c406beb770b96b8c11f65cedcd7e
bohwaz <bohwaz@kd2.org>
2022-08-15 19:38:36 +0000

    Fix disabling of accounting when creating a service for a user
    FossilOrigin-Name: c2737395b8ab9746563680a0e00719ac549fd817428760c0fc48e515e3d75f74

--- src/www/admin/static/scripts/service_form.js
+++ src/www/admin/static/scripts/service_form.js
@@ -33,7 +33,7 @@ function selectFee(elm) {
 
 	if (accounting && create) {
 		$('#f_create_payment_1').checked = true;
-		let btn = $('#f_account_container').firstElementChild;
+		let btn = $('#f_account_selector_container').querySelector('button');
 		btn.value = btn.value.replace(/&year=\d+/, '') + '&year=' + elm.getAttribute('data-year');
 	}
 
