3d3f18e2398957187d2f52f4f4a9837b7f4fa7b1
bohwaz <bohwaz@kd2.org>
2022-10-23 21:36:50 +0000

    Limit what we reset for wrong account codes
    FossilOrigin-Name: f7c7a58e9d58dd5e2ffe8def3f2ccbd6a3f16086baa853a0bb6575ba3d3fb45a

--- src/include/migrations/1.1/31.sql
+++ src/include/migrations/1.1/31.sql
@@ -32,12 +32,14 @@ UPDATE acc_accounts SET position = 5
 	AND position NOT IN (0, 5)
 	AND code LIKE '87_%';
 
--- Remove type from accounts that are outside their class
-UPDATE acc_accounts SET type = 0 WHERE type = 1 AND code NOT LIKE '51%';
-UPDATE acc_accounts SET type = 0 WHERE type = 2 AND code NOT LIKE '53%';
-UPDATE acc_accounts SET type = 0 WHERE type = 3 AND code NOT LIKE '511%';
+-- Change code from accounts that are outside their class
+UPDATE acc_accounts SET code = '9' || code  WHERE type = 7 AND code NOT LIKE '9%';
+
+-- Remove bank for banks that are outside their class
+UPDATE acc_accounts SET type = 0 WHERE type = 1 AND code NOT LIKE '5%';
+UPDATE acc_accounts SET type = 0 WHERE type = 2 AND code NOT LIKE '5%';
+UPDATE acc_accounts SET type = 0 WHERE type = 3 AND code NOT LIKE '5%';
 UPDATE acc_accounts SET type = 0 WHERE type = 4 AND code NOT LIKE '4%';
 UPDATE acc_accounts SET type = 0 WHERE type = 5 AND code NOT LIKE '6%';
 UPDATE acc_accounts SET type = 0 WHERE type = 6 AND code NOT LIKE '7%';
-UPDATE acc_accounts SET type = 0 WHERE type = 7 AND code NOT LIKE '9%';
 UPDATE acc_accounts SET type = 0 WHERE type = 8 AND code NOT LIKE '8%';
