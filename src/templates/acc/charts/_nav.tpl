d7906a8fb6866693a62ac6a7c664445f28315b7b
bohwaz <bohwaz@kd2.org>
2022-10-29 23:47:25 +0000

    Move all chart creation options to main page
    FossilOrigin-Name: c6adc73bacf23424357f028b84255efa53ad5294d8b7639274143771afea69ca

--- src/templates/acc/charts/_nav.tpl
+++ src/templates/acc/charts/_nav.tpl
@@ -6,9 +6,4 @@
 		<li class="current"><a href="{$admin_url}acc/charts/">Plans comptables</a></li>
 	</ul>
 
-	<ul class="sub">
-		<li{if $current == 'charts'} class="current"{/if}><a href="{$admin_url}acc/charts/">Gestion des plans comptables</a></li>
-		<li{if $current == 'install'} class="current"{/if}><a href="{$admin_url}acc/charts/install.php">Installer un plan comptable</a></li>
-		<li{if $current == 'import'} class="current"{/if}><a href="{$admin_url}acc/charts/import.php">Importer un plan comptable personnel</a></li>
-	</ul>
 </nav>
--- src/www/admin/acc/charts/index.php
+++ src/www/admin/acc/charts/index.php
@@ -2,6 +2,7 @@
 namespace Garradin;
 
 use Garradin\Entities\Accounting\Chart;
+use Garradin\Accounting\Accounts;
 use Garradin\Accounting\Charts;
 
 require_once __DIR__ . '/../_inc.php';
@@ -28,9 +29,31 @@ if ($session->canAccess($session::SECTION_ACCOUNTING, $session::ACCESS_ADMIN)) {
 		}
 	}
 
+	$form->runIf('install', function () {
+		Charts::install(f('install'));
+	}, 'acc_charts_new', '!acc/charts/');
+
+
+	$form->runIf('import', function () {
+		$db = DB::getInstance();
+		$db->begin();
+
+		$chart = new Chart;
+		$chart->importForm();
+		$chart->save();
+		$chart->accounts()->importUpload($_FILES['file']); // This will save everything
+
+		$db->commit();
+	}, 'acc_charts_new', '!acc/charts/');
+
+	$tpl->assign('columns', implode(', ', Accounts::EXPECTED_CSV_COLUMNS));
+	$tpl->assign('country_list', Utils::getCountryList());
+
 	$tpl->assign('from', (int)qg('from'));
 	$tpl->assign('charts_groupped', Charts::listByCountry());
 	$tpl->assign('country_list', Utils::getCountryList());
+
+	$tpl->assign('install_list', Charts::listInstallable());
 }
 
 $tpl->display('acc/charts/index.tpl');
