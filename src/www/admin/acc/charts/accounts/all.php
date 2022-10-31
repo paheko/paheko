3bd8a8bf84ceda313d7e6b4e35504ac1bc5ccbb0
bohwaz <bohwaz@kd2.org>
2022-10-31 22:26:49 +0000

    Mark accounts as favorite quickly
    FossilOrigin-Name: 55666f92d67c7410bad143b0622609a2c7c3ff28b90d3f1c58d7c2d693f8859a

--- src/www/admin/acc/charts/accounts/all.php
+++ src/www/admin/acc/charts/accounts/all.php
@@ -21,6 +21,26 @@ if (!$chart) {
 
 $accounts = $chart->accounts();
 
+$form->runIf('bookmark', function () use ($accounts) {
+	$b = f('bookmark');
+
+	if (!is_array($b) || empty($b)) {
+		return;
+	}
+
+	$id = key($b);
+	$value = current($b);
+	$a = $accounts->get($id);
+	$a->bookmark = (bool) $value;
+	$a->save();
+}, null, Utils::getSelfURI());
+
+
+$list = $accounts->list();
+$list->setTitle($chart->label);
+$list->loadFromQueryString();
+
 $tpl->assign('chart', $chart);
-$tpl->assign('accounts', $accounts->listAll());
+$tpl->assign('list', $accounts->list());
+
 $tpl->display('acc/charts/accounts/all.tpl');
