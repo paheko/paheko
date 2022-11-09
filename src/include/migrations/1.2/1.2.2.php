9c3630ffa37ff8d819994c334852ebb22be5e86f
bohwaz <bohwaz@kd2.org>
2022-11-09 16:00:03 +0000

    Change account code for accounts not in 512* but that are probably still bank accounts
    FossilOrigin-Name: 0504e72d19e9224aed4e769e9efe258110452f9f09e352577977fef40adf118c

---
+++ src/include/migrations/1.2/1.2.2.php
@@ -0,0 +1,26 @@
+<?php
+
+// Put back bank accounts that were in 51* but not in 512* back in 512 subclass
+$sql = 'SELECT a.*, c.code AS chart_code, c.country
+	FROM acc_accounts AS a
+	INNER JOIN acc_charts AS c ON c.id = a.id_chart
+	WHERE a.code LIKE \'51%\' AND a.type = 0 AND a.code NOT LIKE \'517%\' AND a.code NOT LIKE \'518%\'
+	AND (SELECT COUNT(*) FROM acc_transactions_lines AS b WHERE b.id_account = a.id) > 0
+	AND c.country = \'FR\'
+	GROUP BY a.id;';
+
+$db->begin();
+
+foreach ($db->iterate($sql) as $row) {
+	$new_code = '5120' . substr($row->code, 2);
+	// Change code
+	$db->preparedQuery('UPDATE acc_accounts SET code = ?, type = 1, user = ? WHERE id = ?;', [$new_code, $row->chart_code ? 1 : 0, $row->id]);
+
+	// If the account was part of the official chart, put it back in the chart
+	if ($row->chart_code && !$row->user) {
+		$db->preparedQuery('INSERT INTO acc_accounts (id_chart, code, label, position, user) VALUES (?, ?, ?, ?, ?);',
+			[$row->id_chart, $row->code, $row->label, $row->position, 0]);
+	}
+}
+
+$db->commit();
