ccdacf534af5878c9ff609e2046d4b3b9d887c6a
bohwaz <bohwaz@kd2.org>
2022-11-16 22:17:24 +0000

    Improve UX of account selector when there are zero accounts of the targeted types
    FossilOrigin-Name: 9f5d88de3192c9d9fee9caa09e98617cfff5d68254d94c186a10da1cd157ad4b

--- src/www/admin/acc/charts/accounts/_inc.php
+++ src/www/admin/acc/charts/accounts/_inc.php
@@ -2,6 +2,8 @@
 
 namespace Garradin;
 
+use Garradin\Entities\Accounting\Account;
+
 require_once __DIR__ . '/../../_inc.php';
 
 // Only open edit/delete/new actions in a dialog if we are not already in a dialog
@@ -9,15 +11,19 @@ $dialog_target = !isset($_GET['_dialog']) ? '_dialog=manage' : null;
 
 $types = null;
 $types_arg = null;
+$types_names = null;
 
 // Filter only some types (if coming from a selector)
 if (qg('types')) {
 	$types = explode(':', qg('types'));
 	$types = array_map('intval', $types);
 	$types_arg = 'types=' . implode(':', $types);
+
+	$types_names = count($types) ? array_intersect_key(Account::TYPES_NAMES, array_flip($types)) : [];
+	$types_names = implode(', ', $types_names);
 }
 
-$tpl->assign(compact('types_arg', 'dialog_target'));
+$tpl->assign(compact('types_arg', 'dialog_target', 'types_names'));
 
 function chart_reload_or_redirect(string $url)
 {
