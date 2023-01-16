0817f7c8b70bd1a71b31447b268413d344854f85
bohwaz <bohwaz@kd2.org>
2023-01-16 15:13:02 +0000

    Store key separately on Windows, as app directory is not writable
    FossilOrigin-Name: a3ec637a31def4333246d76d83d94cc869c536af5974bdd196b04ec9035bf2db

--- build/windows/config.local.php
+++ build/windows/config.local.php
@@ -7,5 +7,16 @@ if (!empty(getenv('LOCALAPPDATA'))) {
 	define('Garradin\DATA_ROOT', trim(getenv('LOCALAPPDATA'), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'Paheko');
 }
 
+// Store secret key in user directory
+if (!defined('Garradin\SECRET_KEY')) {
+	if (file_exists(DATA_ROOT . '/key')) {
+		define('Garradin\SECRET_KEY', trim(file_get_contents(DATA_ROOT . '/key')));
+	}
+	else {
+		define('Garradin\SECRET_KEY', base64_encode(random_bytes(16)));
+		file_put_contents(DATA_ROOT . '/key', SECRET_KEY);
+	}
+}
+
 // Always log in as admin user
 const LOCAL_LOGIN = -1;
