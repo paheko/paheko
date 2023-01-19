8e89b00e4a5899d93997c717667231f25da61b58
alinaar <alinaar@noemail.net>
2023-01-19 18:24:11 +0000

    Module data storing fixed (data update through save() call)
    FossilOrigin-Name: ca62dd132dc110b53bdad7df82a737069522e3138429997df43bc5fa49b74528

--- src/include/lib/Garradin/UserTemplate/Functions.php
+++ src/include/lib/Garradin/UserTemplate/Functions.php
@@ -93,11 +93,11 @@ class Functions
 				$params['key'] = Utils::uuid();
 			}
 
-			$where = 'key = ?';
+			$field = 'key';
 			$where_value = $params['key'];
 		}
 		elseif (!empty($params['id'])) {
-			$where = 'id = ?';
+			$field = 'id';
 			$where_value = $params['id'];
 		}
 		else {
@@ -105,6 +105,7 @@ class Functions
 		}
 
 		$key = $params['key'] ?? null;
+		$id = $params['id'] ?? null;
 
 		unset($params['key'], $params['id']);
 
@@ -118,7 +119,7 @@ class Functions
 		$db = DB::getInstance();
 
 		if ($key == 'config') {
-			$exists = $db->firstColumn(sprintf('SELECT config FROM %s WHERE name = ?;', Module::TABLE), $name);
+			$exists = $db->firstColumn(sprintf('SELECT config AS value FROM %s WHERE name = ?;', Module::TABLE), $name);
 		}
 		else {
 			$db->exec(sprintf('
@@ -129,10 +130,10 @@ class Functions
 				);
 				CREATE UNIQUE INDEX IF NOT EXISTS %1$s_key ON %1$s (key);', $table));
 
-			$exists = $db->first(sprintf('SELECT value FROM %s WHERE %s;', $table, $where), $where_value);
+			$exists = $db->first(sprintf('SELECT value FROM %s WHERE %s;', $table, ($field . ' = ?')), $where_value);
 		}
 
-		$exists = json_decode((string) $exists, true);
+		$exists = json_decode((string) $exists->value, true);
 
 		// Merge before update
 		if ($exists) {
@@ -166,7 +167,7 @@ class Functions
 			$db->insert($table, compact('value', 'key'));
 		}
 		else {
-			$db->update($table, compact('value'), sprintf('%s = :match', $where), ['match' => $where_value]);
+			$db->update($table, compact('value'), sprintf('%s = :match', $field), ['match' => $where_value]);
 		}
 	}
 
