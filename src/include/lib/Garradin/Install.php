b0521b1fdb3e6aeac3861caea09b2b80fa8c9dcb
alinaar <alinaar@noemail.net>
2023-01-26 13:44:01 +0000

    Fix Installation with already existing empty config.local.php (e.g., "touch config.local.php" to set writing permissions to config file only and not to entire src directory)
    FossilOrigin-Name: 47f2de2286d04d07e89f3274701632ba4985355c77eb928db7134b0c5f863aef

--- src/include/lib/Garradin/Install.php
+++ src/include/lib/Garradin/Install.php
@@ -357,7 +357,7 @@ class Install
 		$path = ROOT . DIRECTORY_SEPARATOR . 'config.local.php';
 		$new_line = sprintf('const %s = %s;', $key, var_export($value, true));
 
-		if (file_exists($path)) {
+		if (file_exists($path) AND filesize($path)) {
 			$config = file_get_contents($path);
 
 			$pattern = sprintf('/^.*(?:const\s+%s|define\s*\(.*%1$s).*$/m', $key);
