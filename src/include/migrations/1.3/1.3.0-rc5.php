db260bed1aa0d68be2a4ae5131acf2ed0a80924a
bohwaz <bohwaz@kd2.org>
2023-08-15 14:02:42 +0000

    Try to restore files from previous RC versions if using FileSystem
    FossilOrigin-Name: 4e681ddea313310fe79ddc7ce244cc1a349b21f8623eef0cf91c69c41e251aea

--- src/include/migrations/1.3/1.3.0-rc5.php
+++ src/include/migrations/1.3/1.3.0-rc5.php
@@ -1,3 +1,19 @@
 <?php
 
 $db->import(ROOT . '/include/migrations/1.3/1.3.0-rc5.sql');
+
+if (FILE_STORAGE_BACKEND == 'FileSystem') {
+	rename(FILE_STORAGE_CONFIG, FILE_STORAGE_CONFIG . '.deprecated');
+
+	foreach (Files::all() as $file) {
+		if ($file->isDir()) {
+			continue;
+		}
+
+		// Copy files from old location to new
+		$path = sprintf(FILE_STORAGE_CONFIG . '.deprecated/%.2s/%1$s', md5($file->id()));
+		$modified = clone $file->modified;
+		$file->store(compact('path'));
+		Utils::safe_unlink($path);
+	}
+}
