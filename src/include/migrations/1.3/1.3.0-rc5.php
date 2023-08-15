15cb4d68bea6b68be0862fc90bd3edf9fc182e2d
bohwaz <bohwaz@kd2.org>
2023-08-15 14:11:44 +0000

    Try to get around foreign keys issues
    FossilOrigin-Name: 017a58741b95e4bdc50f358ea3bd58a737fd4ab98e2d889ed7ac2b849a92920f

--- src/include/migrations/1.3/1.3.0-rc5.php
+++ src/include/migrations/1.3/1.3.0-rc5.php
@@ -2,6 +2,7 @@
 
 namespace Paheko;
 
+$db->beginSchemaUpdate();
 $db->import(ROOT . '/include/migrations/1.3/1.3.0-rc5.sql');
 
 if (FILE_STORAGE_BACKEND == 'FileSystem') {
@@ -14,8 +15,10 @@ if (FILE_STORAGE_BACKEND == 'FileSystem') {
 
 		// Copy files from old location to new
 		$path = sprintf(FILE_STORAGE_CONFIG . '.deprecated/%.2s/%1$s', md5($file->id()));
-		$modified = clone $file->modified;
 		$file->store(compact('path'));
 		Utils::safe_unlink($path);
 	}
 }
+
+$db->commitSchemaUpdate();
+
