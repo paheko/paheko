89413062363205a1de041917edc83a7f89c0325e
bohwaz <bohwaz@kd2.org>
2023-09-11 15:40:57 +0000

    Avoid users table error when restoring old database
    FossilOrigin-Name: 012c43747cfa31db0e26d63c1effa4780de7d779e7e7899d6c89e3e58463d513

--- src/include/lib/Paheko/Backup.php
+++ src/include/lib/Paheko/Backup.php
@@ -484,7 +484,7 @@ class Backup
 		}
 
 		// Force user to be re-logged as the first admin
-		if ($session && version_compare($version, '1.1.0', '>=') && $session->isLogged(true) && !$session->refresh()) {
+		if ($session && version_compare($version, '1.3.0', '>=') && $session->isLogged(true) && !$session->refresh()) {
 			$session->forceLogin(-1);
 			$return |= self::CHANGED_USER;
 		}
