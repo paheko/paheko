50d3ae1dfec2f0068b7e78d8115cd17d347b8445
bohwaz <bohwaz@kd2.org>
2023-09-16 09:13:21 +0000

    Fix reset form
    FossilOrigin-Name: 9da6c064066d7cbbf7c3d13c0d4511ca694fa1d2e663ca946ab6b015874cb38c

--- src/www/admin/config/advanced/reset.php
+++ src/www/admin/config/advanced/reset.php
@@ -6,7 +6,7 @@ use Paheko\Install;
 require_once __DIR__ . '/../_inc.php';
 
 $form->runIf('reset_ok', function () use ($session) {
-	Install::reset($session, f('passe_verif'));
+	Install::reset($session, f('checkme'));
 }, 'reset');
 
 $tpl->display('config/advanced/reset.tpl');
