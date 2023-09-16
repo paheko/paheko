e6167245462e7e494981ecdad9a4914fd19f7167
bohwaz <bohwaz@kd2.org>
2023-09-16 09:01:59 +0000

    Make sure we don't rely on document_root for rewrites
    FossilOrigin-Name: dda9f461549f279f7d09b664b18e430f3e3a0cd7308ef9d3a883ef37f8fd5fe2

--- src/www/_route.php
+++ src/www/_route.php
@@ -3,6 +3,7 @@
 namespace Paheko;
 
 use Paheko\Web\Router;
+use Paheko\Email\Emails;
 
 if (empty($_SERVER['REQUEST_URI'])) {
 	http_response_code(500);
@@ -18,22 +19,38 @@ if ('_route.php' === basename($uri)) {
 
 http_response_code(200);
 
-if (($pos = strpos($uri, '?')) !== false)
-{
+if (($pos = strpos($uri, '?')) !== false) {
 	$uri = substr($uri, 0, $pos);
 }
 
-if (file_exists(__DIR__ . $uri))
-{
-	if (PHP_SAPI != 'cli-server') {
-		http_response_code(500);
-		die('Erreur de configuration du serveur web: cette URL ne devrait pas être traitée par Paheko');
-	}
-
+if (PHP_SAPI === 'cli-server' && file_exists(__DIR__ . $uri)) {
 	return false;
 }
-else
-{
-	require __DIR__ . '/../include/init.php';
-	Router::route();
+
+require __DIR__ . '/../include/init.php';
+
+// Handle __un__subscribe URL: .../?un=XXXX
+if (($uri === '/' || $uri === '') && !empty($_GET['un'])) {
+	$params = array_intersect_key($_GET, ['un' => null, 'v' => null]);
+
+	// RFC 8058
+	if (!empty($_POST['Unsubscribe']) && $_POST['Unsubscribe'] == 'Yes') {
+		$email = Emails::getEmailFromOptout($params['un']);
+
+		if (!$email) {
+			throw new UserException('Adresse email introuvable.');
+		}
+
+		$email->setOptout();
+		$email->save();
+		http_response_code(200);
+		echo 'Unsubscribe successful';
+		return;
+	}
+
+	Utils::redirect('!optout.php?' . http_build_query($params));
+	return;
 }
+
+// Call router
+Router::route();
--- src/www/index.php
+++ src/www/index.php
@@ -1,32 +1,5 @@
 <?php
 
-namespace Paheko;
+$_SERVER['REQUEST_URI'] = '/';
 
-use Paheko\Email\Emails;
-use Paheko\Web\Router;
-
-require_once __DIR__ . '/../include/init.php';
-
-// Handle __un__subscribe URL
-if (!empty($_GET['un'])) {
-	$params = array_intersect_key($_GET, ['un' => null, 'v' => null]);
-
-	// RFC 8058
-	if (!empty($_POST['Unsubscribe']) && $_POST['Unsubscribe'] == 'Yes') {
-		$email = Emails::getEmailFromOptout($params['un']);
-
-		if (!$email) {
-			throw new UserException('Adresse email introuvable.');
-		}
-
-		$email->setOptout();
-		$email->save();
-		http_response_code(200);
-		echo 'Unsubscribe successful';
-		exit;
-	}
-
-	Utils::redirect('!optout.php?' . http_build_query($params));
-}
-
-Router::route();
+require __DIR__ . '/_route.php';
