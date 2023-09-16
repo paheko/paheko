086d86c16ef357d227a75c54a90735fdb3033ef5
bohwaz <bohwaz@kd2.org>
2023-09-16 16:42:36 +0000

    Remove protect_contact from brindille modifiers, as it is now in common modifiers
    FossilOrigin-Name: dd8c649003d1ff554c08abb0a614af45390fb6204c03f71c3587060e5dfd77d7

--- src/include/lib/Paheko/UserTemplate/Modifiers.php
+++ src/include/lib/Paheko/UserTemplate/Modifiers.php
@@ -22,7 +22,6 @@ class Modifiers
 		'match',
 		'truncate',
 		'excerpt',
-		'protect_contact',
 		'atom_date',
 		'xml_escape',
 		'json_decode',
@@ -134,27 +133,6 @@ class Modifiers
 		$str = self::truncate($str, $length);
 		$str = preg_replace("/\n{2,}/", '</p><p>', $str);
 		return '<p>' . $str . '</p>';
-	}
-
-	static public function protect_contact(?string $contact, ?string $type = null): string
-	{
-		if (!trim($contact))
-			return '';
-
-		if ($type == 'mail' || strpos($contact, '@')) {
-			$user = strtok($contact, '@');
-			$domain = strtok('.');
-			$ext = strtok(false);
-
-			return sprintf('<a href="#error" class="protected-contact" data-a="%s" data-b="%s" data-c="%s"
-				onclick="if (this.href.match(/#error/)) this.href = [\'mail\', \'to:\', this.dataset.a, \'@\', this.dataset.b, \'.\' + this.dataset.c].join(\'\');"></a>',
-				htmlspecialchars($user), htmlspecialchars($domain), htmlspecialchars($ext));
-		}
-		else {
-			$label = preg_replace_callback('/[a-zA-Z0-9@]/', fn ($match)  => '&#' . ord($match[0]) . ';', htmlspecialchars($contact));
-			$url = htmlspecialchars($type ? $type . ':' : '') . $label;
-			return sprintf('<a href="%s">%s</a>', $url, $label);
-		}
 	}
 
 	static public function atom_date($date)
