430d63c1f5bc867e63c2e0c3e8df4a8ad473cf0b
bohwaz <bohwaz@kd2.org>
2023-09-24 21:04:05 +0000

    Use flatten in class
    FossilOrigin-Name: 511401488161cfc42d1e0c3062d22faca63eed0b8c70e4d2de6491f849385523

--- src/include/migrations/1.3/1.3.0-rc14.php
+++ src/include/migrations/1.3/1.3.0-rc14.php
@@ -2,25 +2,10 @@
 
 namespace Paheko;
 
-use Paheko\Files\Files;
-use Paheko\Entities\Files\File;
+use Paheko\Web\Sync;
 
 $db->beginSchemaUpdate();
 $db->import(ROOT . '/include/migrations/1.3/1.3.0-rc14.sql');
 $db->commitSchemaUpdate();
 
-// Flatten file hierarchy
-function flatten_web_dir(string $path)
-{
-	foreach (Files::list($path) as $file) {
-		if ($file->isDir()) {
-			$flatten($file->path);
-
-			if (substr_count($file->path, '/') >= 2) {
-				$file->rename(File::CONTEXT_WEB . '/' . $file->name);
-			}
-		}
-	}
-}
-
-flatten_web_dir(File::CONTEXT_WEB);
+Sync::flatten();
