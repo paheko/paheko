b6e0e2322f1a0dafe953bd87e24ce25f55ec9b5c
bohwaz <bohwaz@kd2.org>
2023-10-17 12:21:55 +0000

    Make sure we are returning something that works if the admin has selected no indexed fields as user name
    FossilOrigin-Name: 682a04fd5350249b4a5f09c41b275def5f4c8292b84acda1786f07fb70033bc5

--- src/include/lib/Paheko/Users/DynamicFields.php
+++ src/include/lib/Paheko/Users/DynamicFields.php
@@ -161,14 +161,15 @@ class DynamicFields
 			$fields[] = $field->name;
 		}
 
+		// There are no indexed fields in the name, eg. only the user number, then discard the index
+		if (!count($fields)) {
+			return '1';
+		}
+
 		$db = DB::getInstance();
 
 		if ($prefix) {
 			$fields = array_map(fn($v) => $prefix . '.' . $db->quoteIdentifier($v), $fields);
-		}
-
-		if (count($fields) == 1) {
-			return $fields[0];
 		}
 
 		foreach ($fields as &$field) {
