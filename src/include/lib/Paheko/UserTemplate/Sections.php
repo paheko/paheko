b66e0683f75d859aa79aeff623c4cd181e81f7a2
bohwaz <bohwaz@kd2.org>
2023-10-28 11:38:06 +0000

    Fix sub-queries in list
    FossilOrigin-Name: 48e6cd41704074f84ba690f2616da81feed60890869acadcdd0f4969a689d1f3

--- src/include/lib/Paheko/UserTemplate/Sections.php
+++ src/include/lib/Paheko/UserTemplate/Sections.php
@@ -482,7 +482,7 @@ class Sections
 			foreach (explode(';', $params['select']) as $i => $c) {
 				$c = trim($c);
 
-				$pos = strpos($c, ' AS ');
+				$pos = strrpos($c, ' AS ');
 
 				if ($pos) {
 					$select = trim(substr($c, 0, $pos));
