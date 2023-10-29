01bc9256eb20fdca69ce6798e708fd41aa924123
bohwaz <bohwaz@kd2.org>
2023-10-29 20:09:36 +0000

    Input function: handle case when option is NULL
    FossilOrigin-Name: 689017808862c4a9e7cc5b8f4da18a3129fd2bbcadf3b7505a10bba3d4e08fd2

--- src/include/lib/Paheko/UserTemplate/CommonFunctions.php
+++ src/include/lib/Paheko/UserTemplate/CommonFunctions.php
@@ -264,7 +264,7 @@ class CommonFunctions
 
 			if (null !== $current_value && (is_array($current_value) || is_object($current_value))) {
 				foreach ($current_value as $v => $l) {
-					if (trim($l) === '') {
+					if (empty($l) || trim($l) === '') {
 						continue;
 					}
 
