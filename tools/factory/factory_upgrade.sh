8eadf2f0eb76fc9b44f0902812475281e25c9f22
bohwaz <bohwaz@kd2.org>
2023-10-31 12:26:21 +0000

    Update factory scripts for Paheko
    FossilOrigin-Name: 1e2186970a9398bbb76ccfe6973447eb831ee96dbcc1950acab7d7a21b283224

--- tools/factory/factory_upgrade.sh
+++ tools/factory/factory_upgrade.sh
@@ -4,11 +4,11 @@
 # veiller à ce que ce soit le même que dans config.local.php
 FACTORY_USER_DIRECTORY="users"
 
-# Chemin vers le script upgrade.php de Garradin
-GARRADIN_UPGRADE_SCRIPT="scripts/cron.php"
+# Chemin vers le script upgrade.php de Paheko
+PAHEKO_UPGRADE_SCRIPT="scripts/cron.php"
 
 for user in $(cd ${FACTORY_USER_DIRECTORY} && ls -1d */)
 do
-	GARRADIN_FACTORY_USER=$(basename "$user")
-	php $GARRADIN_UPGRADE_SCRIPT
+	PAHEKO_FACTORY_USER=$(basename "$user")
+	php $PAHEKO_UPGRADE_SCRIPT
 done
