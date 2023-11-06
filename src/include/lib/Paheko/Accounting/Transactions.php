7a44b32fc7a774f8d9e52cf26e877b35a8f14024
bohwaz <bohwaz@kd2.org>
2023-11-06 15:13:16 +0000

    GROUP transactions, as a user can be linked multiple times for each transaction, either as an activity or as user-entered link
    FossilOrigin-Name: 540334a108df6a734756d015be79aa9de1f1c5cac68769e3d07ec64d4fa0fb7b

--- src/include/lib/Paheko/Accounting/Transactions.php
+++ src/include/lib/Paheko/Accounting/Transactions.php
@@ -93,7 +93,7 @@ class Transactions
 
 	static public function countForUser(int $user_id): int
 	{
-		return DB::getInstance()->count('acc_transactions_users', 'id_user = ?', $user_id);
+		return (int) DB::getInstance()->firstColumn('SELECT COUNT(*) FROM acc_transactions_users WHERE id_user = ? GROUP BY id_transaction;', $user_id);
 	}
 
 	static public function countForCreator(int $user_id): int
