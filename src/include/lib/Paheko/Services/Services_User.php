81c3e37cb91fb8df0c9d676850bb6e42bf317d75
bohwaz <bohwaz@kd2.org>
2023-12-20 22:11:22 +0000

    Try to fix matching fee with service in import
    FossilOrigin-Name: 27552eb341925da873037e5acc7c8c98c36ba38c2bebf16b1983a4ef1d1fb977

--- src/include/lib/Paheko/Services/Services_User.php
+++ src/include/lib/Paheko/Services/Services_User.php
@@ -144,18 +144,14 @@ class Services_User
 
 				if (!empty($row->fee)) {
 					foreach ($fees as $fee) {
-						if (strcasecmp($fee->label, $row->fee) === 0) {
+						if (strcasecmp($fee->label, $row->fee) === 0 && $fee->id === $id_service) {
 							$id_fee = $fee->id;
 							break;
 						}
 					}
 
 					if (!$id_fee) {
-						throw new UserException(sprintf('Le tarif d\'activité "%s" n\'existe pas', $row->fee));
-					}
-
-					if ($fee->id_service !== $id_service) {
-						throw new UserException(sprintf('Le tarif d\'activité "%s" ne correspond pas à l\'activité "%s"', $fee->label, $row->service));
+						throw new UserException(sprintf('Le tarif "%s" n\'existe pas pour cette activité', $row->fee));
 					}
 
 					$id_fee = $fee->id;
