c550631f836e6a6997e6a7bdafffc6c364128c58
bohwaz <bohwaz@kd2.org>
2024-01-20 15:36:14 +0000

    Fix tests
    FossilOrigin-Name: 84a04d4cfd2622c9241369280921d16a2f422b2b1aae62fd254f206000d504b9

--- tests/unit_tests/01_basic/db.php
+++ tests/unit_tests/01_basic/db.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Garradin;
+namespace Paheko;
 
 use KD2\Test;
 
--- tests/unit_tests/01_basic/paths.php
+++ tests/unit_tests/01_basic/paths.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace Garradin;
+namespace Paheko;
 use KD2\Test;
 
 require_once INIT;
 
-Test::assert(defined('Garradin\ROOT'));
+Test::assert(defined('Paheko\ROOT'));
 Test::assert(is_readable(ROOT));
 
-Test::assert(defined('Garradin\PLUGINS_ROOT'));
+Test::assert(defined('Paheko\PLUGINS_ROOT'));
 
-Test::assert(defined('Garradin\DATA_ROOT'));
+Test::assert(defined('Paheko\DATA_ROOT'));
 
-Test::assert(defined('Garradin\CACHE_ROOT'));
+Test::assert(defined('Paheko\CACHE_ROOT'));
