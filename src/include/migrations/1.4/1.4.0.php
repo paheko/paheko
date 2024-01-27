a11c11666ef59ab2e9a3e02ffed822425e518898
bohwaz <bohwaz@kd2.org>
2024-01-27 23:04:50 +0000

    Rename 1.3.6 to 1.4.0
    FossilOrigin-Name: 86f0e08fbdee71e5ef552d2da9047ce2ec01001fca9bf8d964c8a2fdb8bfc512

--- src/include/migrations/1.4/1.4.0.php
+++ src/include/migrations/1.4/1.4.0.php
@@ -7,6 +7,6 @@ use Paheko\Accounting\Charts;
 Charts::resetRules(['FR']);
 
 $db->beginSchemaUpdate();
-$db->import(ROOT . '/include/migrations/1.3/1.3.6.sql');
+$db->import(ROOT . '/include/migrations/1.4/1.4.0.sql');
 $db->commitSchemaUpdate();
 
