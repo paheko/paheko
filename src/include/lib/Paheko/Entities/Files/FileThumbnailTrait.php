1dfab28b0083d79428c95c67f10e17d3857e0cb1
bohwaz <bohwaz@kd2.org>
2024-02-03 21:35:49 +0000

    Don't report mupdf errors when the PDF is password-protected or corrupted, also use a better replacement SVG for missing thumbnails
    FossilOrigin-Name: 6f578461b1345479361b2b7abff0988187471c1a39a2ec4897c7b12f2dfc30d6

--- src/include/lib/Paheko/Entities/Files/FileThumbnailTrait.php
+++ src/include/lib/Paheko/Entities/Files/FileThumbnailTrait.php
@@ -12,7 +12,7 @@ use Paheko\UserException;
 use Paheko\Utils;
 use Paheko\Web\Cache as Web_Cache;
 
-use const Paheko\{DOCUMENT_THUMBNAIL_COMMANDS, WOPI_DISCOVERY_URL, CACHE_ROOT, WWW_URL, BASE_URL};
+use const Paheko\{DOCUMENT_THUMBNAIL_COMMANDS, WOPI_DISCOVERY_URL, CACHE_ROOT, WWW_URL, BASE_URL, ADMIN_URL};
 
 trait FileThumbnailTrait
 {
@@ -339,7 +339,17 @@ trait FileThumbnailTrait
 				// Don't trust code as it can return != 0 even if generation was OK
 				if (!file_exists($destination) || filesize($destination) < 10) {
 					Utils::safe_unlink($destination);
-					throw new \RuntimeException($command . ' execution failed with code: ' . $code . "\n" . $output);
+					$e = new \RuntimeException($command . ' execution failed with code: ' . $code . "\n" . $output);
+
+					if ($command === 'mupdf' && $code === 0) {
+						// MuPDF can fail for a number of reasons: password protection, broken document, etc.
+						// Usually when error is zero it means that calling mupdf works, but not the conversion
+						// so just ignore it, no need to report it, just log it
+						ErrorManager::logException($e);
+						return null;
+					}
+
+					throw $e;
 				}
 			}
 		}
@@ -387,19 +397,19 @@ trait FileThumbnailTrait
 		file_put_contents($destination, $out);
 	}
 
-	protected function createThumbnail(string $size, string $destination): void
+	protected function createThumbnail(string $size, string $destination): bool
 	{
 		$operations = self::ALLOWED_THUMB_SIZES[$size];
 
 		if ($this->extension() === 'md' || $this->extension() === 'txt') {
 			$this->createSVGThumbnail($operations, $destination);
-			return;
+			return true;
 		}
 
 		$i = $this->asImageObject();
 
 		if (!$i) {
-			return;
+			return false;
 		}
 
 		// Always autorotate first
@@ -429,6 +439,7 @@ trait FileThumbnailTrait
 		}
 
 		$i->save($destination, $format);
+		return true;
 	}
 
 	/**
@@ -449,11 +460,15 @@ trait FileThumbnailTrait
 
 		if (!Static_Cache::exists($cache_id)) {
 			try {
-				$this->createThumbnail($size, $destination);
+				$success = $this->createThumbnail($size, $destination);
 			}
 			catch (\RuntimeException $e) {
 				ErrorManager::reportExceptionSilent($e);
-				header('Content-Type: image/svg+xml', true);
+				$success = false;
+			}
+
+			if (!$success) {
+				header('Content-Type: image/svg+xml; charset=utf-8', true);
 				echo $this->getMissingThumbnail($size);
 				return;
 			}
@@ -482,21 +497,13 @@ trait FileThumbnailTrait
 	{
 		$w = preg_replace('/[^\d]+/', '', $size) ?: 150;
 		$h = intval($w / (2/3));
-		return sprintf('<svg width="%d" height="%d" version="1.1" viewBox="0 0 210 297" xmlns="http://www.w3.org/2000/svg">
-			<g stroke="#808080">
-				<g fill="none" stroke-width="8">
-					<path d="m27.44 40.23 155.1 0.1495"/>
-					<path d="m27.44 76.33 155.1 0.1495"/>
-					<path d="m27.44 110.2 155.1 0.1495"/>
-					<path d="m27.44 148.2 155.1 0.1495"/>
-					<path d="m27.44 182.2 155.1 0.1495"/>
-					<path d="m87.08 268.9 94.9 0.1505"/>
-					<path d="m87.35 225 94.9 0.1505"/>
+
+		return sprintf('<svg width="%d" height="%d" version="1.1" viewBox="0 0 396.9 396.9" xmlns="http://www.w3.org/2000/svg">
+				<g transform="translate(-28.52 -6.592)" fill="#ccc" stroke="none">
+					<path d="m264.7 148.4v-69.85q3.44 2.117 5.556 4.233l60.06 60.32q2.117 1.852 4.233 5.292zm-18.79 4.762q0 5.821 4.233 9.79 4.233 3.969 10.05 4.233h80.17v155.8q0 6.085-3.969 10.05t-10.05 4.233h-198.4q-6.085 0-10.05-4.233-3.969-4.233-4.233-10.05v-236q0-6.085 4.233-10.05 4.233-3.969 10.05-4.233h118z" />
 				</g>
-				<g fill="#808080" stop-color="#000000" stroke-width="71.02" style="text-orientation:upright" aria-label="?">
-					<path d="m49.23 255.1q-0.04893-1.762-0.04893-2.642 0-5.187 1.468-8.954 1.076-2.838 3.474-5.725 1.762-2.104 6.312-6.116 4.599-4.061 5.97-6.459 1.37-2.398 1.37-5.236 0-5.138-4.012-9.003-4.012-3.914-9.835-3.914-5.627 0-9.395 3.523t-4.942 11.01l-9.052-1.076q1.223-10.03 7.242-15.36 6.067-5.333 16-5.333 10.52 0 16.78 5.725t6.263 13.85q0 4.697-2.202 8.661-2.202 3.963-8.612 9.639-4.306 3.817-5.627 5.627-1.321 1.81-1.957 4.159-0.6361 2.349-0.734 7.633zm-0.5382 17.66v-10.03h10.03v10.03z" stroke="none"/>
-				</g>
-			</g>
-		</svg>', $w, $h);
+			</svg>',
+			$w, $h
+		);
 	}
 }
