d76d17225694e6de210e1879a4b9d42424bea7e3
bohwaz <bohwaz@kd2.org>
2024-03-22 01:10:13 +0000

    Restore email verify time window
    FossilOrigin-Name: c02662923120f94d7b13111edd391df0d7e37c69530a1f9d2c89703aa9e79ee5

--- src/www/admin/users/mailing/verify.php
+++ src/www/admin/users/mailing/verify.php
@@ -10,6 +10,17 @@ $session->requireAccess($session::SECTION_USERS, $session::ACCESS_WRITE);
 $address = qg('address');
 $email = Emails::getOrCreateEmail($address);
 
+if (!$email->canSendVerificationAfterFail()) {
+	if ($email->optout) {
+		$message = 'Il n\'est pas possible de renvoyer une vérification à cette adresse pour le moment, il faut attendre 3 jours.';
+	}
+	else {
+		$message = 'Il n\'est pas possible de renvoyer une vérification à cette adresse pour le moment, il faut attendre un mois.';
+	}
+
+    throw new UserException($message);
+}
+
 $csrf_key = 'send_verification';
 
 $form->runIf('send', function () use ($email, $address) {
