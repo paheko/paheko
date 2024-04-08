926b1fde801df7af0aae924684034a586f3b063f
bohwaz <bohwaz@kd2.org>
2024-04-08 17:50:52 +0000

    Fix in case string is invalid unicode
    FossilOrigin-Name: f657a02228c28ee5060a69c5f961c73724e67ba7d52e79499e35a4c75ab12a68

--- src/include/lib/Paheko/UserTemplate/CommonModifiers.php
+++ src/include/lib/Paheko/UserTemplate/CommonModifiers.php
@@ -335,8 +335,12 @@ class CommonModifiers
 
 	static public function typo($str, $locale = 'fr')
 	{
+		if (empty($str)) {
+			return $str;
+		}
+
 		$str = preg_replace('/[\h]*([?!:»;])(?=\s|$)/us', "\xc2\xa0\\1", $str);
-		$str = preg_replace('/(?<=^|\s)([«])[\h]*/u', "\\1\xc2\xa0", $str);
+		$str = preg_replace('/(?<=^|\s)([«])[\h]*/u', "\\1\xc2\xa0", (string)$str);
 		return $str;
 	}
 
