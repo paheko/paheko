939458982b9189d13136a9e17848575207b5a882
bohwaz <bohwaz@kd2.org>
2024-05-21 12:26:24 +0000

    Fix list of years in quick search form
    FossilOrigin-Name: 5c1d5fdbc7bc4f3d4b59018ed41fa838ca733c5c9c6d22c8f207e221ad333cc7

--- src/www/admin/acc/index.php
+++ src/www/admin/acc/index.php
@@ -18,7 +18,7 @@ $tpl->assign('graphs', array_slice(Graph::URL_LIST, 0, 3));
 $years = Years::listOpen(true);
 $tpl->assign('years', $years);
 $tpl->assign('first_year', count($years) ? current($years)->id : null);
-$tpl->assign('all_years', [null => '-- Tous les exercices'] + Years::listAssoc());
+$tpl->assign('all_years', Years::listAssoc());
 $tpl->assign('last_transactions', Years::listLastTransactions(10, $years));
 
 $tpl->display('acc/index.tpl');
