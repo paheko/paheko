073c65d81518da6dd8a95313c883429b2308a44c
bohwaz <bohwaz@kd2.org>
2024-08-06 22:51:45 +0000

    Fix phpstan config
    FossilOrigin-Name: c83e6f5bcdd76cf4f65c6d346e7e98c7d63c0bfe791572f29e1dc9118b41879f

--- tests/phpstan.config.php
+++ tests/phpstan.config.php
@@ -2,7 +2,7 @@
 
 namespace Garradin;
 
-if (!defined('\Garradin\WWW_URL')) {
-	define('Garradin\WWW_URL', 'http://localhost/');
-	define('Garradin\WWW_URI', '/');
+if (!defined('\Paheko\WWW_URL')) {
+	define('Paheko\WWW_URL', 'http://localhost/');
+	define('Paheko\WWW_URI', '/');
 }
