32bf8df7e28dee5ce564dbd458d0cb36a9536b76
bohwaz <bohwaz@kd2.org>
2024-08-07 12:05:37 +0000

    Remove unused method
    FossilOrigin-Name: 11ef52211d9e382a45919c1ba21fd774c0d2ee27d6e8ceeafa082af397d3875a

--- src/include/lib/Paheko/Accounting/Charts.php
+++ src/include/lib/Paheko/Accounting/Charts.php
@@ -68,21 +68,6 @@ class Charts
 		}
 	}
 
-	static public function installCountryDefault(string $country_code): Chart
-	{
-		if ($country_code == 'CH') {
-			$chart_code = 'ch_asso';
-		}
-		elseif ($country_code == 'BE') {
-			$chart_code = 'be_pcmn_2019';
-		}
-		else {
-			$chart_code = 'fr_pca_2018';
-		}
-
-		return self::install($chart_code);
-	}
-
 	static public function install(string $chart_code): Chart
 	{
 		if (!array_key_exists($chart_code, self::BUNDLED_CHARTS)) {
