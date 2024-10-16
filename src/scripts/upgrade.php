23672c7b57465e9de6925c2bbfafc7afeba8c5ab
bohwaz <bohwaz@kd2.org>
2024-10-16 13:32:24 +0000

    Add mention of correct command
    FossilOrigin-Name: bad07a115f154ab006b6ea2a16bba282dfe8cb8ff78f34814c576d48df44d64f

--- src/scripts/upgrade.php
+++ src/scripts/upgrade.php
@@ -1,6 +1,6 @@
 <?php
 
-fwrite(STDERR, "This command is deprecated, please use bin/paheko instead\n"); //FIXME 1.4
+fwrite(STDERR, "This command is deprecated, please use 'bin/paheko upgrade' instead\n"); //FIXME 1.4
 
 $_SERVER['argv'] = ['paheko', 'upgrade'];
 require __DIR__ . '/../bin/paheko';
