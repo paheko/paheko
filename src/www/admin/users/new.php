c37b170166fee006452a018bace598c7aa1d3c70
bohwaz <bohwaz@kd2.org>
2025-01-13 18:44:14 +0000

    Allow to execute a js function when creating a new user (hack for the cash register plugin)
    FossilOrigin-Name: f069255c781ce30c7678d547a3991f420b2bc5e46babc306c3bc1b71aaefc7ac

--- src/www/admin/users/new.php
+++ src/www/admin/users/new.php
@@ -30,6 +30,15 @@ $form->runIf('save', function () use ($user, $session, &$is_duplicate) {
 	}
 
 	$user->save();
+
+	if (!empty($_GET['tab'])) {
+		printf('<!DOCTYPE html><script type="text/javascript">window.parent.renameTabUser(%d, %s);</script>Redirection en cours...',
+			$user->id(),
+			json_encode($user->name())
+		);
+		exit;
+	}
+
 	Utils::redirect('!users/details.php?id=' . $user->id());
 }, $csrf_key);
 
