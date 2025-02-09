889eba28d206de6f8de3f80f8902d374c08b7df0
bohwaz <bohwaz@kd2.org>
2025-02-09 18:36:35 +0000

    Implement method to convert RGB color to HSL
    FossilOrigin-Name: 178577fc8356603454b1bf7c9954c30807802ced2f8c4c1037c5d045989f85f9

--- src/include/lib/Paheko/Utils.php
+++ src/include/lib/Paheko/Utils.php
@@ -1138,6 +1138,52 @@ class Utils
 		return array($h * 360, $s, $l);
 	}
 
+    static public function hsl2rgb (int $h, int $s, int $l): string
+    {
+        $h /= 60;
+        if ($h < 0) $h = 6 - fmod(-$h, 6);
+        $h = fmod($h, 6);
+
+        $s = max(0, min(1, $s / 100));
+        $l = max(0, min(1, $l / 100));
+
+        $c = (1 - abs((2 * $l) - 1)) * $s;
+        $x = $c * (1 - abs(fmod($h, 2) - 1));
+
+        if ($h < 1) {
+            $r = $c;
+            $g = $x;
+            $b = 0;
+        } elseif ($h < 2) {
+            $r = $x;
+            $g = $c;
+            $b = 0;
+        } elseif ($h < 3) {
+            $r = 0;
+            $g = $c;
+            $b = $x;
+        } elseif ($h < 4) {
+            $r = 0;
+            $g = $x;
+            $b = $c;
+        } elseif ($h < 5) {
+            $r = $x;
+            $g = 0;
+            $b = $c;
+        } else {
+            $r = $c;
+            $g = 0;
+            $b = $x;
+        }
+
+        $m = $l - $c / 2;
+        $r = round(($r + $m) * 255);
+        $g = round(($g + $m) * 255);
+        $b = round(($b + $m) * 255);
+
+        return '#' . dechex($r) . dechex($g) . dechex($b);
+    }
+
 	static public function HTTPCache(?string $hash, ?int $last_change, int $max_age = 3600, bool $immutable = false): bool
 	{
 		$etag = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH'], '"\' ') : null;
