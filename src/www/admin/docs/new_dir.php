a4efd5ae8a9299e46d9cc904cd2b82e1a7db7d15
bohwaz <bohwaz@kd2.org>
2025-02-17 17:42:50 +0000

    Allow to create directories inside paths that don't exist yet
    FossilOrigin-Name: 747ad584c6f0393f94d2f32115119eb3cc4935e3e9da0a73bcc68cf83fdb065e

--- src/www/admin/docs/new_dir.php
+++ src/www/admin/docs/new_dir.php
@@ -8,17 +8,24 @@ use Paheko\Users\Session;
 
 require_once __DIR__ . '/_inc.php';
 
-$parent = Files::get(qg('p'));
 $session = Session::getInstance();
+$parent = $_GET['p'] ?? '';
 
-if (!$parent || !$parent->canCreateDirHere($session)) {
+if (empty($parent)
+	|| !File::canCreateDir($parent . '/example', $session)) {
 	throw new UserException('Vous n\'avez pas le droit de créer de répertoire ici.', 403);
 }
 
 $csrf_key = 'create_dir';
 
-$form->runIf('create', function () use ($parent, $session) {
-	$dir = $parent->mkdir((string) f('name'), $session);
+$form->runIf('create', function () use ($session, $parent) {
+	$path = $parent . '/' . ($_POST['name'] ?? '');
+
+	if (!File::canCreateDir($parent . '/example', $session)) {
+		throw new UserException('Vous n\'avez pas le droit de créer de répertoire ici.', 403);
+	}
+
+	$dir = Files::mkdir($path);
 
 	$url = '!docs/?path=' . $dir->path;
 
