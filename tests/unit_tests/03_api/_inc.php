5587bb719768519d463cc91102647bb346b8dce3
bohwaz <bohwaz@kd2.org>
2025-09-15 14:45:08 +0000

    Add new tests for web API
    FossilOrigin-Name: 3b60c306c5df78a8862be39743282b440f5530c3825948f562f9b68c8eae83a3

--- tests/unit_tests/03_api/_inc.php
+++ tests/unit_tests/03_api/_inc.php
@@ -7,10 +7,11 @@ use KD2\Test;
 
 paheko_init();
 
-function api(string $method, string $path, array $params = [])
+function api(string $method, string $path, array $params = [], ?string $input = null)
 {
 	$api = new API($method, $path, $params, false);
 	Test::isInstanceOf(API::class, $api);
 	$api->setAccessLevelByName('admin');
+	$api->setInput($input);
 	return $api->route();
 }
--- tests/unit_tests/03_api/web.php
+++ tests/unit_tests/03_api/web.php
@@ -31,6 +31,30 @@ Test::isInstanceOf(\stdClass::class, $c);
 Test::assert(isset($c->title));
 Test::assert($c->title === 'Test coucou');
 
+$c = api('PUT', 'web/test-new/coucou.txt', [], 'coucou!!!');
+Test::isArray($c);
+Test::assert(count($c) > 0);
+$c = (object) $c;
+
+Test::assert($c->success === true);
+
+$c = api('GET', 'web/test-new/attachments');
+Test::isArray($c);
+Test::assert(count($c) > 0);
+$c = current($c);
+Test::isArray($c);
+Test::assert(isset($c['name']));
+Test::assert($c['name'] === 'coucou.txt');
+Test::assert(isset($c['size']));
+Test::assert($c['size'] === 9);
+
+$c = api('DELETE', 'web/test-new/coucou.txt');
+Test::isArray($c);
+Test::assert(count($c) > 0);
+$c = (object) $c;
+
+Test::assert($c->success === true);
+
 $c = api('DELETE', 'web/test-new');
 Test::isArray($c);
 Test::assert(count($c) > 0);
