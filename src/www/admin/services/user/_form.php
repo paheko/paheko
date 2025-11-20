94d377a1dd60a9ec88dc1b8f350e94ae8cbab97f
bohwaz <bohwaz@kd2.org>
2025-11-20 00:43:59 +0000

    Remove "old services", replace with "archived services" (backport from dev branch)
    FossilOrigin-Name: 17c2055cabff8194f29e5ea90cf6ab59e5d79e2e7ba2ef24eac83718dd37ace1

--- src/www/admin/services/user/_form.php
+++ src/www/admin/services/user/_form.php
@@ -12,26 +12,13 @@ if (!defined('\Paheko\ROOT')) {
 
 assert(isset($tpl, $form_url, $create));
 
-$current_only = !f('past_services');
-
 // If there is only one user selected we can calculate the amount
 $single_user_id = isset($users) && count($users) == 1 ? key($users) : null;
 $copy_service ??= null;
 $copy_service_only_paid ??= null;
 $users ??= null;
 
-$grouped_services = Services::listGroupedWithFees($single_user_id, (int)$current_only);
-
-if (!count($grouped_services)) {
-	$current_only = false;
-	$grouped_services = Services::listGroupedWithFees($single_user_id, (int)$current_only);
-}
-
-if (!isset($count_all)) {
-	$count_all = Services::count();
-}
-
-$has_past_services = count($grouped_services) != $count_all;
+$grouped_services = Services::listGroupedWithFees($single_user_id);
 
 $today = new \DateTime;
 
@@ -39,7 +26,7 @@ $tpl->assign([
 	'custom_js' => ['service_form.js'],
 ]);
 
-$tpl->assign(compact('form_url', 'today', 'grouped_services', 'current_only', 'has_past_services',
+$tpl->assign(compact('form_url', 'today', 'grouped_services',
 	'create', 'copy_service', 'copy_service_only_paid'));
 
 $tpl->assign_by_ref('users', $users);
