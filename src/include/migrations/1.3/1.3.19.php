f47620d848c24a5a9ca18949a916a6599ec5bb71
bohwaz <bohwaz@kd2.org>
2025-12-31 15:50:07 +0000

    Fix some type issues in other accounting charts
    FossilOrigin-Name: cbbe85a00d3a725b273ac7bd6ce85ccc6ac3d78e55a417474970c6ec076795b5

---
+++ src/include/migrations/1.3/1.3.19.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Paheko;
+use Paheko\Accounting\Charts;
+
+$db->beginSchemaUpdate();
+// Make sure 120 and 129 do exist
+Charts::updateInstalled('fr_pcc_2020');
+
+// Make sure 690/692 have the correct type
+Charts::updateInstalled('be_pcmn_2019');
+$db->commitSchemaUpdate();
