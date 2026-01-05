d31114e8208b4149419a9ba6932acbc807886046
bohwaz <bohwaz@kd2.org>
2026-01-05 03:56:00 +0000

    Reset local rules
    FossilOrigin-Name: 12a07370fb3ad3756f2a12fee19a58a29059da4c1df9461212df66b60b6fd454

--- src/include/migrations/1.3/1.3.19.php
+++ src/include/migrations/1.3/1.3.19.php
@@ -9,4 +9,7 @@ Charts::updateInstalled('fr_pcc_2020');
 
 // Make sure 690/692 have the correct type
 Charts::updateInstalled('be_pcmn_2019');
+
+// Make sure rules are applied correctly
+Charts::resetRules(['FR', 'BE']);
 $db->commitSchemaUpdate();
