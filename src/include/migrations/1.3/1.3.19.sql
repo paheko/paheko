cb3a5977b32d203ec88734824f9e1d1c83f37f83
bohwaz <bohwaz@kd2.org>
2026-02-01 15:04:34 +0000

    We don't need to create this index twice
    FossilOrigin-Name: 520ca16d406399989778e2a1ad89bfd5c7a6ecb8afce2dee20ab7e6b5eeef95f

--- src/include/migrations/1.3/1.3.19.sql
+++ src/include/migrations/1.3/1.3.19.sql
@@ -3,9 +3,6 @@ DELETE FROM acc_transactions_users WHERE id_service_user IS NULL AND rowid NOT I
 	SELECT MIN(rowid) FROM acc_transactions_users WHERE id_service_user IS NULL GROUP BY id_transaction, id_user
 );
 
--- Create index
-CREATE UNIQUE INDEX IF NOT EXISTS acc_transactions_users_unique ON acc_transactions_users (id_user, id_transaction, COALESCE(id_service_user, 0));
-
 ALTER TABLE acc_transactions_users RENAME TO acc_transactions_users_old;
 DROP INDEX IF EXISTS acc_transactions_users_service;
 DROP INDEX IF EXISTS acc_transactions_users_unique;
