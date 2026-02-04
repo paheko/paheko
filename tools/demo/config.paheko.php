53853b217b92c12396ffc0b6af6116b932446e18
bohwaz <bohwaz@kd2.org>
2026-02-04 21:44:54 +0000

    Fix plugins and modules paths
    FossilOrigin-Name: 23731a24e25d1b7c4c86a1db0b62c0c0ef7807e47f4305f3de64e39d1c4f040c

--- tools/demo/config.paheko.php
+++ tools/demo/config.paheko.php
@@ -34,7 +34,7 @@ require __DIR__ . '/functions.php';
 const SHOW_ERRORS = false;
 const USE_CRON = true;
 
-const PLUGINS_ROOT = __DIR__ . '/src/plugins';
+const PLUGINS_ROOT = __DIR__ . '/src/paheko-plugins';
 
 const ENABLE_TECH_DETAILS = false;
 const ENABLE_UPGRADES = false;
