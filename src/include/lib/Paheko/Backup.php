d79b54272bd48dc9a4762189fadad8992777c59d
bohwaz <bohwaz@kd2.org>
2026-02-27 22:10:17 +0000

    We don't need to use CACHE_ROOT for backup file if we use VACUUM INTO
    FossilOrigin-Name: 4689c89311c4b09ce287928afbf3a7160d417524de405bb38df4b7c4cc141d31

--- src/include/lib/Paheko/Backup.php
+++ src/include/lib/Paheko/Backup.php
@@ -213,12 +213,10 @@ class Backup
 	{
 		$db = DB::getInstance();
 
-		// Use a temporary file so that if BACKUPS_ROOT is on NFS,
-		// we don't have issues with SQLite writing over NFS
-		$tmp = tempnam(CACHE_ROOT, 'sqlite-backup-');
-
 		// use VACUUM INTO when SQLite 3.27+ is available
 		if ($db->hasFeatures('vacuum_into')) {
+			$tmp = tempnam(Utils::dirname($destination), 'sqlite-backup-');
+
 			// We need to allow ATTACH here, as VACUUM INTO is using ATTACH,
 			// which is restricted for security reasons, so we disable the authorizer
 			DB::toggleAuthorizer($db, false);
@@ -226,14 +224,14 @@ class Backup
 			$db->exec(sprintf('VACUUM INTO %s;', $db->quote($tmp)));
 
 			DB::toggleAuthorizer($db, true);
-
-			// Make sure the backup file has DELETE journal mode so the WAL file is squashed
-			$dest_db = new \SQLite3($tmp);
-			$dest_db->exec('PRAGMA journal_mode = DELETE;');
 		}
+		// use ::backup since PHP 7.4.0+
+		// https://www.php.net/manual/en/sqlite3.backup.php
 		else {
-			// use ::backup since PHP 7.4.0+
-			// https://www.php.net/manual/en/sqlite3.backup.php
+			// Use a temporary file so that if BACKUPS_ROOT is on NFS,
+			// we don't have issues with SQLite writing over NFS
+			$tmp = tempnam(CACHE_ROOT, 'sqlite-backup-');
+
 			$dest_db = new \SQLite3($tmp);
 			$dest_db->createCollation('U_NOCASE', [Utils::class, 'unicodeCaseComparison']);
 
