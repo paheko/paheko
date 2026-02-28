502f00a8baa31e04e912b7ca0f31ac731932de35
bohwaz <bohwaz@kd2.org>
2026-02-28 00:13:40 +0000

    Don't use flex, it's worse
    FossilOrigin-Name: 562068adf8f2c098017c6068a80cde0539f4967479c3d55b053fe0b03c4656cb

--- tools/demo/config.paheko.php
+++ tools/demo/config.paheko.php
@@ -144,11 +144,10 @@ if (!empty($_POST['delete_demo']) && $_POST['delete_demo'] === $delete_hash) {
 }
 
 $message = <<<EOF
-<div style="display: flex; flex-wrap: wrap; justify-content: space-between">
-<p>Compte de test temporaire</p>
-<p>L'envoi d'e-mail est désactivé</p>
-<p style="color: darkred; font-weight: bold">Toutes les données seront effacées au bout de {$days} jours&nbsp;!</strong>
-<form method="post" style="display: inline" onsubmit="return confirm('Supprimer le compte de test ?');"><button type="submit" name="delete_demo" value="{$delete_hash}" style="border: 1px solid #999; padding: 1px 2px; background: none; font: inherit; font-size: .8em">Supprimer</button></form>
+Compte de test temporaire
+— L'envoi d'e-mail est désactivé
+— <strong style="color: darkred">Toutes les données seront effacées au bout de {$days} jours&nbsp;!</strong>
+— <form method="post" style="display: inline; float: right" onsubmit="return confirm('Supprimer le compte de test ?');"><button type="submit" name="delete_demo" value="{$delete_hash}" style="border: 1px solid #999; padding: 2px 4px; background: none; font: inherit; font-size: .8em">Supprimer</button></form>
 EOF;
 
 define('Paheko\ALERT_MESSAGE', $message);
