5b58933a00bd6a51c4c0d3af6d37498f750f6544
bohwaz <bohwaz@kd2.org>
2026-03-02 21:37:49 +0000

    Import changes from dev for API test
    FossilOrigin-Name: 6e791d693359560bfa24b6e51eb5da30defdc2328e12387f9e6616d1b4110f61

--- tests/unit_tests/03_api/user.php
+++ tests/unit_tests/03_api/user.php
@@ -4,6 +4,9 @@ namespace Paheko;
 
 use KD2\Test;
 
+use Paheko\Entities\Services\Service;
+use Paheko\Entities\Services\Fee;
+
 require __DIR__ . '/_inc.php';
 
 $c = api('GET', 'user/categories');
@@ -22,3 +25,26 @@ $c = (object) $c;
 Test::assert(isset($c->nom));
 Test::strictlyEquals('Coucou', $c->nom);
 Test::assert(isset($c->id));
+
+$id = $c->id;
+
+$service = new Service;
+$service->importForm(['label' => 'Adhésion test', 'duration' => '365']);
+$service->save();
+
+$fee = new Fee;
+$fee->importForm(['label' => 'Tarif réduit', 'amount' => '10', 'id_service' => $service->id()]);
+$fee->save();
+
+$c = api('POST', 'user/' . $id . '/subscribe', ['id_service' => $service->id(), 'id_fee' => $fee->id(), 'paid' => true]);
+
+Test::isArray($c);
+Test::assert(count($c) > 0);
+$c = (object) $c;
+
+Test::assert(isset($c->id));
+Test::strictlyEquals(1000, $c->expected_amount);
+Test::strictlyEquals(true, $c->paid);
+Test::strictlyEquals($service->id(), $c->id_service);
+Test::strictlyEquals($fee->id(), $c->id_fee);
+Test::assert($c->expiry_date->format('Ymd') == $c->date->modify('+365 days')->format('Ymd'));
