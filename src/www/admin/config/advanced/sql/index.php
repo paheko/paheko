04f15a2e4088a713a4e6a49ed8ef40f61b5b7433
bohwaz <bohwaz@kd2.org>
2026-04-12 23:33:21 +0000

    Allow to filter list of tables
    FossilOrigin-Name: da820c680ba330767d8bf1412fe342a112789d7dd519d8382745b848012269bf

--- src/www/admin/config/advanced/sql/index.php
+++ src/www/admin/config/advanced/sql/index.php
@@ -5,6 +5,16 @@ require_once __DIR__ . '/../../_inc.php';
 
 $db = DB::getInstance();
 $tables_list = $db->getTablesList();
+$filter = $_GET['filter'] ?? null;
+$where = $filter ? sprintf(' AND tbl_name LIKE %s', $db->quote($filter . '%')) : '';
+
+if ($filter) {
+	foreach ($tables_list as $name => $date) {
+		if (!str_starts_with($name, $filter)) {
+			unset($tables_list[$name]);
+		}
+	}
+}
 
 foreach ($tables_list as $name => &$data) {
 	$data->count = $db->count($name);
@@ -13,9 +23,9 @@ foreach ($tables_list as $name => &$data) {
 
 unset($data);
 
-$tpl->assign('index_list',$db->getAssoc('SELECT name, sql FROM sqlite_master WHERE type = \'index\' AND name NOT LIKE \'sqlite_%\' ORDER BY name;'));
-$tpl->assign('triggers_list', $db->getAssoc('SELECT name, sql FROM sqlite_master WHERE type = \'trigger\' ORDER BY name;'));
+$index_list = $db->getAssoc(sprintf('SELECT name, sql FROM sqlite_master WHERE type = \'index\' AND name NOT LIKE \'sqlite_%%\' %s ORDER BY name;', $where));
+$triggers_list = $db->getAssoc(sprintf('SELECT name, sql FROM sqlite_master WHERE type = \'trigger\' %s ORDER BY name;', $where));
 
-$tpl->assign(compact('tables_list'));
+$tpl->assign(compact('tables_list', 'index_list', 'triggers_list'));
 
 $tpl->display('config/advanced/sql/index.tpl');
