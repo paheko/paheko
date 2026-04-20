f5ef01492b78e7cfb6fa7024b319e0b6e69bc1af
bohwaz <bohwaz@kd2.org>
2026-04-20 16:32:28 +0000

    Validate parent path before trying upload
    FossilOrigin-Name: 7bea3bf304a5d15b30e30e0ae23e2d773e97d21b2403a97acc62a99efd17c790

--- src/www/admin/common/files/upload.php
+++ src/www/admin/common/files/upload.php
@@ -7,21 +7,23 @@ use Paheko\Users\Session;
 
 require __DIR__ . '/../../_inc.php';
 
-$parent = qg('p');
+$path = $_GET['p'] ?? null;
 
-if (!File::canCreate($parent)) {
+File::validatePath($path);
+
+if (!File::canCreate($path)) {
 	throw new UserException('Vous n\'avez pas le droit de créer de fichier ici.', 403);
 }
 
 $csrf_key = 'upload_file';
 
-$form->runIf('upload', function () use ($parent) {
-	Files::uploadMultiple($parent, 'file', Session::getInstance());
-}, $csrf_key, '!docs/?path=' . $parent);
+$form->runIf('upload', function () use ($path) {
+	Files::uploadMultiple($path, 'file', Session::getInstance());
+}, $csrf_key, '!docs/?path=' . $path);
 
-$max = (int) qg('max');
+$max = intval($_GET['max'] ?? 1);
 $multiple = $max > 1;
 
-$tpl->assign(compact('parent', 'csrf_key', 'multiple'));
+$tpl->assign(compact('csrf_key', 'multiple'));
 
 $tpl->display('common/files/upload.tpl');
