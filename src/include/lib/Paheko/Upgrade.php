3f2d1b5c657d6a36137db72485e324416a687c26
bohwaz <bohwaz@kd2.org>
2026-06-04 13:35:20 +0000

    Add column in upgrade
    FossilOrigin-Name: 95a0bf80d31dfb024112b396a9886f18bd08b4123b1c4de646ac7de00f0e58c4

--- src/include/lib/Paheko/Upgrade.php
+++ src/include/lib/Paheko/Upgrade.php
@@ -159,6 +159,12 @@ class Upgrade
 				$db->commitSchemaUpdate();
 			}
 
+			if (version_compare($v, '1.3.21', '<')) {
+				$db->beginSchemaUpdate();
+				$db->import(ROOT . '/include/migrations/1.3/1.3.21.sql');
+				$db->commitSchemaUpdate();
+			}
+
 			Plugins::upgradeAllIfRequired();
 
 			// Vérification de la cohérence des clés étrangères
