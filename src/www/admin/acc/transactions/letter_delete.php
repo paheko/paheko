6f0eda648d21bfdd2e69e56a74404486a22b130e
bohwaz <bohwaz@kd2.org>
2026-08-02 21:09:49 +0000

    Add missing requireAccess statement
    FossilOrigin-Name: f5ca01cfa8ed86c02cd54cf9e8747bd3dbac43c33a71bb08b628164ff79046da

--- src/www/admin/acc/transactions/letter_delete.php
+++ src/www/admin/acc/transactions/letter_delete.php
@@ -6,6 +6,8 @@ use Paheko\Accounting\Transactions;
 
 require_once __DIR__ . '/../_inc.php';
 
+$session->requireAccess($session::SECTION_ACCOUNTING, $session::ACCESS_WRITE);
+
 $current_year->assertCanBeModified();
 
 $csrf_key = 'acc_delete_letter';
