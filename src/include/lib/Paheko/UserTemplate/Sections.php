4455372834ca718540359c38e06b3e39ab0a8046
bohwaz <bohwaz@kd2.org>
2026-08-31 10:14:36 +0000

    Fix parsing of double quotes in #select blocks
    FossilOrigin-Name: 384289814f69d802b079efe9379ce4f2e5d96d6056191faa3c07fba4abb5ea0a

--- src/include/lib/Paheko/UserTemplate/Sections.php
+++ src/include/lib/Paheko/UserTemplate/Sections.php
@@ -76,7 +76,7 @@ class Sections
 	static public function selectStart(string $name, string $sql, UserTemplate $tpl, int $line): string
 	{
 		// Split string at semicolon, unless in comment or inside balanced quotes
-		$regexp = "/'(?:''|[^'])*'(*SKIP)(*F)|\"(?:\\\"|[^\"])*\"(*SKIP)(*F)|--[^\\r\\n]*(*SKIP)(*F)|;/";
+		$regexp = "/'(?:''|[^'])*'(*SKIP)(*F)|\"(?:\"\"|[^\"])*\"(*SKIP)(*F)|--[^\\r\\n]*(*SKIP)(*F)|;/";
 		$parts = preg_split($regexp, $sql, 2);
 
 		$sql = trim($parts[0] ?? '');
