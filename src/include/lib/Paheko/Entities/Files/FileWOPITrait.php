275f6b436daf96ddccc9ddb3296cc1e330ffd755
bohwaz <bohwaz@kd2.org>
2026-09-01 17:25:24 +0000

    Fix: use mime, not type
    FossilOrigin-Name: 1d524466000467e3442e36f3550cb472ea175a2c8c28024f40e1389e14952414

--- src/include/lib/Paheko/Entities/Files/FileWOPITrait.php
+++ src/include/lib/Paheko/Entities/Files/FileWOPITrait.php
@@ -62,7 +62,7 @@ trait FileWOPITrait
 	public function getWOPIEditorHTML(?Session $session = null, bool $readonly = false, bool $frame_only = false): ?string
 	{
 		// Never use WOPI editor for internal preview types (eg. text/plain)
-		if (in_array($this->type, self::PREVIEW_TYPES, true)) {
+		if (in_array($this->mime, self::PREVIEW_TYPES, true)) {
 			return null;
 		}
 
